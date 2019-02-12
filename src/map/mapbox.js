b43a30e396ed1901338089e058842e0150d0b043
thinkinggis <lzx199065@gmail.com>
2019-02-12 20:21:14 +0800

    feat(map): add mapbox

--- src/map/mapbox.js
+++ src/map/mapbox.js
@@ -3,22 +3,27 @@ import Util from '../util';
 import { scene } from '../global';
 import * as THREE from '../core/three';
 const WORLD_SIZE = 512;
-export class MapBoxProvider extends Base {
+const MERCATOR_A = 6378137.0;
+const WORLD_SCALE = 1.0;
+const PROJECTION_WORLD_SIZE = WORLD_SIZE / (MERCATOR_A * Math.PI) / 2;
+export default class MapBox extends Base {
   getDefaultCfg() {
     return Util.assign(scene, {
       resizeEnable: true,
       viewMode: '3D'
     });
   }
-  constructor(container, cfg, engine) {
+  static project(lnglat) {
+    const d = Math.PI / 180;
+    const x = -MERCATOR_A * lnglat[0] * d * PROJECTION_WORLD_SIZE;
+    const y = -MERCATOR_A * Math.log(Math.tan((Math.PI * 0.25) + (0.5 * lnglat[1] * d))) * PROJECTION_WORLD_SIZE;
+    return { x, y };
+  }
+  constructor(cfg) {
     super(cfg);
-    this.container = container;
-    this.engine = engine;
-    const scene = engine._scene;
+    this.container = this.get('container');
     this.initMap();
     this.addOverLayer();
-    scene.position.x = scene.position.y = WORLD_SIZE / 2;
-    scene.matrixAutoUpdate = false;
     setTimeout(() => {
       this.emit('mapLoad');
     }, 100);
@@ -28,15 +33,24 @@ export class MapBoxProvider extends Base {
   initMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoibHp4dWUiLCJhIjoiYnhfTURyRSJ9.Ugm314vAKPHBzcPmY1p4KQ';
     this.map = new mapboxgl.Map(this._attrs);
+  }
+  asyncCamera(engine) {
+    this.engine = engine;
+    const camera = engine._camera;
+    const scene = engine.world;
+    camera.matrixAutoUpdate = false;
+    scene.position.x = scene.position.y = WORLD_SIZE / 2;
+    scene.matrixAutoUpdate = false;
+    scene.autoUpdate = false;
+    this.updateCamera();
     this.map.on('move', () => {
       this.updateCamera();
     });
-    this.updateCamera();
   }
-  asyncCamera() {
+  updateCamera() {
     const engine = this.engine;
+    const scene = engine.world;
     const camera = engine._camera;
-    const scene = engine._scene;
     // Build a projection matrix, paralleling the code found in Mapbox GL JS
     const fov = 0.6435011087932844;
     const cameraToCenterDistance = 0.5 / Math.tan(fov / 2) * this.map.transform.height;
@@ -49,24 +63,21 @@ export class MapBoxProvider extends Base {
 
     // Add a bit extra to avoid precision problems when a fragment's distance is exactly `furthestDistance`
     const farZ = furthestDistance * 1.01;
-
+    const { x, y } = this.map.transform.point;
     camera.projectionMatrix = this.makePerspectiveMatrix(fov, this.map.transform.width / this.map.transform.height, 1, farZ);
-
-
     const cameraWorldMatrix = new THREE.Matrix4();
     const cameraTranslateZ = new THREE.Matrix4().makeTranslation(0, 0, cameraToCenterDistance);
     const cameraRotateX = new THREE.Matrix4().makeRotationX(this.map.transform._pitch);
     const cameraRotateZ = new THREE.Matrix4().makeRotationZ(this.map.transform.angle);
 
+    // const cameraTranslateCenter = new THREE.Matrix4().makeTranslation(0, 0, cameraToCenterDistance);
     // Unlike the Mapbox GL JS camera, separate camera translation and rotation out into its world matrix
     // If this is applied directly to the projection matrix, it will work OK but break raycasting
     cameraWorldMatrix
         .premultiply(cameraTranslateZ)
         .premultiply(cameraRotateX)
         .premultiply(cameraRotateZ);
-
     camera.matrixWorld.copy(cameraWorldMatrix);
-
 
     const zoomPow = this.map.transform.scale;
     // Handle scaling and translation of objects in the map in the world's matrix transform, not the camera
@@ -82,7 +93,6 @@ export class MapBoxProvider extends Base {
         .makeTranslation(-this.map.transform.x, this.map.transform.y, 0);
     rotateMap
         .makeRotationZ(Math.PI);
-
     scene.matrix = new THREE.Matrix4();
     scene.matrix
         .premultiply(rotateMap)
@@ -117,8 +127,15 @@ export class MapBoxProvider extends Base {
     const canvasContainer = document.getElementById(this.container);
     this.canvasContainer = canvasContainer;
     this.renderDom = document.createElement('div');
-    this.renderDom.style.cssText += 'position: absolute;top: 0; z-index:1;height: 100%;width: 100%;pointer-events: none;';
+    this.renderDom.style.cssText += 'position: absolute;top: 0; z-index:10;height: 100%;width: 100%;pointer-events: none;';
     this.renderDom.id = 'l7_canvaslayer';
     canvasContainer.appendChild(this.renderDom);
+  }
+  mixMap(scene) {
+    const map = this.map;
+    scene.getZoom = () => { return map.getZoom(); };
+    scene.getCenter = () => { return map.getCenter(); };
+    scene.getPitch = () => { return map.getPitch(); };
+
   }
 }
