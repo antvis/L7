712e983b3e37f760ae4575695ab940de32f238f5
thinkinggis <lzx199065@gmail.com>
2019-02-22 12:03:01 +0800

    refactor(core): layer register

--- src/layer/index.js
+++ src/layer/index.js
@@ -1,3 +1,17 @@
+import { registerLayer } from './factory';
+import PolygonLayer from './polygonLayer';
+import PointLayer from './pointLayer';
+import LineLayer from './lineLayer';
+import ImageLayer from './imageLayer';
+import RasterLayer from './rasterLayer';
+
+registerLayer('PolygonLayer', PolygonLayer);
+registerLayer('PointLayer', PointLayer);
+registerLayer('LineLayer', LineLayer);
+registerLayer('ImageLayer', ImageLayer);
+registerLayer('RasterLayer', RasterLayer);
+
+export { LAYER_MAP } from './factory';
 export { default as PolygonLayer } from './polygonLayer';
 export { default as PointLayer } from './pointLayer';
 export { default as LineLayer } from './lineLayer';
