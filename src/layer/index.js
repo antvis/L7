eec6a121ac4c1a13f3442d6285533395a70bbcf6
mipha.ly@alibaba-inc.com <mipha.ly@alibaba-inc.com>
2019-02-27 17:14:01 +0800

    feat(l7): feat heatmap layer

--- src/layer/index.js
+++ src/layer/index.js
@@ -5,14 +5,12 @@ import LineLayer from './lineLayer';
 import ImageLayer from './imageLayer';
 import RasterLayer from './rasterLayer';
 import HeatmapLayer from './heatmapLayer';
-import HeatMapLayer from './heatmap';
 
 registerLayer('PolygonLayer', PolygonLayer);
 registerLayer('PointLayer', PointLayer);
 registerLayer('LineLayer', LineLayer);
 registerLayer('ImageLayer', ImageLayer);
 registerLayer('RasterLayer', RasterLayer);
-registerLayer('HeatMapLayer', HeatMapLayer);
 registerLayer('HeatmapLayer', HeatmapLayer);
 
 export { LAYER_MAP } from './factory';
