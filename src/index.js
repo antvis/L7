b67f14fdea145999fee469a372d20b6bfcb68e31
thinkinggis <lzx199065@gmail.com>
2019-03-01 15:00:48 +0800

    fix package

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import Scene from './core/scene';
 import Global from './global';
 
 const version = Global.version;
-const track = function(enable) {
-  Global.trackable = enable;
-};
-import './track';
 export {
   version,
-  Scene,
-  track
-
+  Scene
 };
 
