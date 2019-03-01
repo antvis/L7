1d4e27d674c7680dd802fd40896b0d9d0f4d3580
mipha.ly <mipha.ly@alibaba-inc.com>
2019-03-01 11:15:33 +0800

    移除three不必要的模块basic material

--- src/core/three.js
+++ src/core/three.js
@@ -23,7 +23,6 @@ export { Vector4 } from 'three/src/math/Vector4.js';
 export { Vector3 } from 'three/src/math/Vector3.js';
 export { Vector2 } from 'three/src/math/Vector2.js';
 export { ShaderMaterial } from 'three/src/materials/ShaderMaterial.js';
-export { MeshBasicMaterial } from 'three/src/materials/MeshBasicMaterial.js';
 export { DataTexture } from 'three/src/textures/DataTexture.js';
 export { Color } from 'three/src/math/Color.js';
 export {
