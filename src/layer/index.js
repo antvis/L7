1d127950193b98e6a0b5bbbaf30a614f5f3a3d22
thinkinggis <lzx199065@gmail.com>
2019-03-01 13:09:41 +0800

    fix layer zoom

--- src/layer/index.js
+++ src/layer/index.js
@@ -4,14 +4,14 @@ import PointLayer from './pointLayer';
 import LineLayer from './lineLayer';
 import ImageLayer from './imageLayer';
 import RasterLayer from './rasterLayer';
-// import HeatmapLayer from './heatmapLayer';
+import HeatmapLayer from './heatmapLayer';
 
 registerLayer('PolygonLayer', PolygonLayer);
 registerLayer('PointLayer', PointLayer);
 registerLayer('LineLayer', LineLayer);
 registerLayer('ImageLayer', ImageLayer);
 registerLayer('RasterLayer', RasterLayer);
-// registerLayer('HeatmapLayer', HeatmapLayer);
+registerLayer('HeatmapLayer', HeatmapLayer);
 
 export { LAYER_MAP } from './factory';
 
