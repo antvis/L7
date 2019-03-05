879535f3f8d970868520cef2ee7da1d4eec4a2d9
thinkinggis <lzx199065@gmail.com>
2019-03-05 21:06:12 +0800

    feat(layer) add fitbounds

--- src/util.js
+++ src/util.js
@@ -1,8 +1,3 @@
-/**
- * @fileOverview The util method based on the lodash.
- * @author dxq613@gmail.com
- * @see https://github.com/lodash/lodash
- */
 import Utils from '@antv/util';
 
 const Util = Utils.mix({}, Utils, {
@@ -13,33 +8,7 @@ const Util = Utils.mix({}, Utils, {
   isNaN,
   snapEqual: Utils.isNumberEqual,
   remove: Utils.pull,
-  inArray: Utils.contains,
-  /**
-   * 将用户输入的 padding 转换成 [top, right, bottom, right] 的模式
-   * @param  {Number|Array} padding 输入的padding
-   * @return {Array} 四个padding 值
-   */
-  toAllPadding(padding) {
-    let top = 0;
-    let left = 0;
-    let right = 0;
-    let bottom = 0;
-
-    if (Util.isNumber(padding) || Util.isString(padding)) {
-      top = left = right = bottom = padding;
-    } else if (Util.isArray(padding)) {
-      top = padding[0];
-      right = !Util.isNil(padding[1]) ? padding[1] : padding[0];
-      bottom = !Util.isNil(padding[2]) ? padding[2] : padding[0];
-      left = !Util.isNil(padding[3]) ? padding[3] : right;
-    } else if (Util.isObject(padding)) {
-      top = padding.top || 0;
-      right = padding.right || 0;
-      bottom = padding.bottom || 0;
-      left = padding.left || 0;
-    }
-    return [ top, right, bottom, left ];
-  }
+  inArray: Utils.contains
 });
 
 Util.Array = {
