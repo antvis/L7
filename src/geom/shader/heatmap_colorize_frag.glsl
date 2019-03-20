9c4029b8b9b7cf92e5e735b9d2845ad9a722ec71
zhengxue.lzx <zhengxue.lzx@alipay.com>
2019-03-20 17:23:28 +0800

    fix(Amap): fix map

--- src/geom/shader/heatmap_colorize_frag.glsl
+++ src/geom/shader/heatmap_colorize_frag.glsl
@@ -6,4 +6,6 @@ void main(){
     float intensity = texture2D(u_texture,v_uv).r;
     vec4 color = texture2D(u_colorRamp,vec2(0.5,1.0-intensity));
     gl_FragColor = color;
+    gl_FragColor.a = color.a * smoothstep(0.,0.05,intensity);
+    
 }
