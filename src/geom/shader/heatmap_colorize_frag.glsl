b23a1ffb4c53a1804499f11dc51778a210976d6a
thinkinggis <lzx199065@gmail.com>
2019-03-22 10:42:54 +0800

    fix(source)

--- src/geom/shader/heatmap_colorize_frag.glsl
+++ src/geom/shader/heatmap_colorize_frag.glsl
@@ -1,11 +1,12 @@
 uniform sampler2D u_texture;
 uniform sampler2D u_colorRamp;
+  uniform float u_opacity;
 varying vec2 v_uv;
 
 void main(){
     float intensity = texture2D(u_texture,v_uv).r;
     vec4 color = texture2D(u_colorRamp,vec2(0.5,1.0-intensity));
     gl_FragColor = color;
-    gl_FragColor.a = color.a * smoothstep(0.,0.05,intensity);
+    gl_FragColor.a = color.a * smoothstep(0.,0.05,intensity) * u_opacity;
     
 }
