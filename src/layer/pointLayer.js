d88c16d95f22066a56b1aad4d7ea530d4c10f635
thinkinggis <lzx199065@gmail.com>
2019-03-26 16:42:30 +0800

    refactor(text):升级文本渲染方法

--- src/layer/pointLayer.js
+++ src/layer/pointLayer.js
@@ -1,6 +1,6 @@
 import Layer from '../core/layer';
 import * as drawPoint from '../layer/render/point';
-import TextBuffer2 from '../geom/buffer/point/text';
+import TextBuffer from '../geom/buffer/point/text';
 import DrawText from './render/point/drawText';
 import Global from '../global';
 // import PointBuffer from '../geom/buffer/point';
@@ -14,26 +14,22 @@ const { pointShape } = Global;
  */
 
 export default class PointLayer extends Layer {
-
   render() {
     this.type = 'point';
-
     this.init();
     if (!this._hasRender) {
       this._prepareRender(this.shapeType);
       this._hasRender = true;
     } else {
       this._initAttrs();
-      (this._needUpdateFilter || this._needUpdateColor) ? this._updateFilter() : null;
+      this._needUpdateFilter || this._needUpdateColor
+        ? this._updateFilter()
+        : null;
     }
     return this;
   }
   _prepareRender() {
     const { stroke, fill } = this.get('styleOptions');
-    if (this.shapeType === 'text') { // 绘制文本图层
-      this._textPoint();
-      return;
-    }
     const style = this.get('styleOptions');
     const activeOption = this.get('activedOptions');
     const config = {
@@ -41,36 +37,55 @@ export default class PointLayer extends Layer {
       activeColor: activeOption.fill
     };
     const pointShapeType = this._getShape();
-
     switch (pointShapeType) {
-      case 'fill' :// 填充图形
-        {
-          if (fill !== 'none') { // 是否填充
-            const attributes = PointBuffer.FillBuffer(this.layerData, style);
-            const meshfill = drawPoint.DrawFill(attributes, config);
-            this.add(meshfill);
-          }
-          if (stroke !== 'none') { // 是否绘制边界
-            const lineAttribute = PointBuffer.StrokeBuffer(this.layerData, style);
-            const meshStroke = drawPoint.DrawStroke(lineAttribute, config);
-            this.add(meshStroke, 'line');
-          }
-          break;
+      case 'fill': { // 填充图形
+        if (fill !== 'none') {
+          // 是否填充
+          const attributes = PointBuffer.FillBuffer(this.layerData, style);
+          const meshfill = drawPoint.DrawFill(attributes, config);
+          this.add(meshfill);
         }
-      case 'image':// 绘制图片标注
-        {
-          const imageAttribute = PointBuffer.ImageBuffer(this.layerData, { imagePos: this.scene.image.imagePos });
-          const imageMesh = drawPoint.DrawImage(imageAttribute, { ...style, texture: this.scene.image.texture });
-          this.add(imageMesh);
-          break;
+        if (stroke !== 'none') {
+          // 是否绘制边界
+          const lineAttribute = PointBuffer.StrokeBuffer(this.layerData, style);
+          const meshStroke = drawPoint.DrawStroke(lineAttribute, config);
+          this.add(meshStroke, 'line');
         }
-      case 'normal' : // 原生点
-        {
-          const normalAttribute = PointBuffer.NormalBuffer(this.layerData, style);
-          const normalPointMesh = drawPoint.DrawNormal(normalAttribute, config);
-          this.add(normalPointMesh);
-          break;
-        }
+        break;
+      }
+      case 'image': { // 绘制图片标注
+        const imageAttribute = PointBuffer.ImageBuffer(this.layerData, {
+          imagePos: this.scene.image.imagePos
+        });
+        const imageMesh = drawPoint.DrawImage(imageAttribute, {
+          ...style,
+          texture: this.scene.image.texture
+        });
+        this.add(imageMesh);
+        break;
+      }
+      case 'normal': { // 原生点
+        const normalAttribute = PointBuffer.NormalBuffer(this.layerData, style);
+        const normalPointMesh = drawPoint.DrawNormal(normalAttribute, config);
+        this.add(normalPointMesh);
+        break;
+      }
+      case 'text': { // 原生点
+        const { width, height } = this.scene.getSize();
+        const textCfg = {
+          ...style,
+          width,
+          height,
+          activeColor: activeOption.fill
+        };
+        const buffer = new TextBuffer(
+          this.layerData,
+          this.scene.fontAtlasManager
+        );
+        const mesh = new DrawText(buffer, textCfg);
+        this.add(mesh);
+        break;
+      }
       default:
         return null;
     }
@@ -87,32 +102,14 @@ export default class PointLayer extends Layer {
         break;
       }
     }
-    if (pointShape['2d'].indexOf(shape) !== -1 || pointShape['3d'].indexOf(shape) !== -1) {
+    if (
+      pointShape['2d'].indexOf(shape) !== -1 ||
+      pointShape['3d'].indexOf(shape) !== -1
+    ) {
       return 'fill';
-    } else if (shape === 'text') {
-      return 'text';
     } else if (this.scene.image.imagesIds.indexOf(shape) !== -1) {
       return 'image';
     }
-    return 'normal';
-
-
+    return 'text';
   }
-  _textPoint() {
-    const styleOptions = this.get('styleOptions');
-    const activeOption = this.get('activedOptions');
-    const { width, height } = this.scene.getSize();
-    const textCfg = {
-      ...styleOptions,
-      width,
-      height,
-      activeColor: activeOption.fill
-    };
-    const buffer2 = new TextBuffer2(this.layerData, this.scene.fontAtlasManager);
-    const testMesh = new DrawText(buffer2, textCfg);
-    this.add(testMesh);
-    return;
-  }
-
 }
-
