556335b6d9d7a356cf7e45251b1d619ed0c56472
thinkinggis <lzx199065@gmail.com>
2019-04-02 11:47:38 +0800

    refactor(linelayer): remove normal line

--- src/layer/render/line/drawMeshLine.js
+++ src/layer/render/line/drawMeshLine.js
@@ -1,7 +1,7 @@
 import * as THREE from '../../../core/three';
 import { MeshLineMaterial } from '../../../geom/material/lineMaterial';
-export default function DrawLine(attributes, style) {
-  const { opacity, zoom, animate, duration, interval, trailLength } = style;
+export default function DrawLine(attributes, cfg, layer) {
+  const { style, animateOptions, activeOption, zoom } = cfg;
   const geometry = new THREE.BufferGeometry();
   geometry.setIndex(attributes.indexArray);
   geometry.addAttribute('pickingId', new THREE.Float32BufferAttribute(attributes.pickingIds, 1));
@@ -11,33 +11,34 @@ export default function DrawLine(attributes, style) {
   geometry.addAttribute('normal', new THREE.Float32BufferAttribute(attributes.normal, 3));
   geometry.addAttribute('a_miter', new THREE.Float32BufferAttribute(attributes.miter, 1));
   geometry.addAttribute('a_distance', new THREE.Float32BufferAttribute(attributes.attrDistance, 1));
+
   const lineMaterial = new MeshLineMaterial({
-    u_opacity: opacity,
+    u_opacity: style.opacity,
     u_zoom: zoom,
-    u_duration: duration,
-    u_interval: interval,
-    u_trailLength: trailLength,
-    u_time: 0
+    u_time: 0,
+    activeColor: activeOption.fill
   }, {
     SHAPE: false,
-    ANIMATE: animate
+    ANIMATE: false
   });
-  const arcMesh = new THREE.Mesh(geometry, lineMaterial);
-  if (animate) {
-    this.scene.startAnimate();
+
+  const lineMesh = new THREE.Mesh(geometry, lineMaterial);
+  if (animateOptions.enable) {
+    layer.scene.startAnimate();
     const {
-      duration,
-      interval,
-      trailLength,
+      duration = 2,
+      interval = 0.5,
+      trailLength = 0.5,
       repeat = Infinity
-    } = style;
-    this.animateDuration =
-    this.scene._engine.clock.getElapsedTime() + duration * repeat;
+    } = animateOptions;
+    layer.animateDuration =
+    layer.scene._engine.clock.getElapsedTime() + duration * repeat;
     lineMaterial.upDateUninform({
       u_duration: duration,
       u_interval: interval,
       u_trailLength: trailLength
     });
+    lineMaterial.setDefinesvalue('ANIMATE', true);
   }
-  return arcMesh;
+  layer.add(lineMesh);
 }
