fe0e4c053d446f05a448b25fbc076e1066ae767c
thinkinggis <lzx199065@gmail.com>
2019-05-27 14:14:45 +0800

    fix(tilelayer): point pickup

--- src/source/parser/geojson.js
+++ src/source/parser/geojson.js
@@ -11,20 +11,20 @@ export default function geoJSON(data, cfg) {
   turfMeta.flattenEach(data, (currentFeature, featureIndex) => { // 多个polygon 拆成一个
     const coord = getCoords(currentFeature);
     let id = featureIndex + 1;
-    // if (cfg.idField) {
-    //   const value = currentFeature.properties[cfg.idField];
-    //  // id = value;
-    //   id = BKDRHash(value) % 1000019;
-    //   if (featureKeys[id] && featureIndex !== featureKeys[id]) {
-    //     // TODO 哈希冲突解决方法
-    //     console.log('哈希冲突', value);
-    //   }
-    //   featureKeys[id] = featureIndex;
-    // }
+    if (cfg.idField && currentFeature.properties[cfg.idField]) {
+      const value = currentFeature.properties[cfg.idField];
+     // id = value;
+      id = BKDRHash(value) % 1000019;
+      // if (featureKeys[id] && featureIndex !== featureKeys[id]) {
+      //   // TODO 哈希冲突解决方法
+      //   console.log('哈希冲突', value);
+      // }
+    }
+    featureKeys[id] = featureIndex;
     const dataItem = {
       ...currentFeature.properties,
       coordinates: coord,
-      _id: currentFeature.properties[cfg.idField]
+      _id: id
     };
     resultData.push(dataItem);
   });
