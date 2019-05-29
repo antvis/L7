1ffc709370f270f62182790ea6e76a6f079a3b79
xiaoiver <pyqiverson@gmail.com>
2019-05-29 20:37:23 +0800

    feat(point-layer): vertex compression

--- src/geom/shader/circle_vert.glsl
+++ src/geom/shader/circle_vert.glsl
@@ -1,6 +1,4 @@
-attribute float a_radius;
-attribute vec2 a_shape;
-attribute vec4 a_color;
+attribute vec4 a_packed_data;
 
 uniform float u_zoom : 1;
 uniform float u_stroke_width : 2;
@@ -11,24 +9,44 @@ varying vec3 v_data;
 varying vec4 v_color;
 varying float v_radius;
 
-void main() {
-  v_color = a_color;
-  v_radius = a_radius;
+#pragma include "decode"
 
-  // extrude
+void main() {
+  // unpack color(vec2)
+  v_color = decode_color(a_packed_data.xy);
+  // unpack picking_id
+  float picking_id = a_packed_data.w;
+
+  // unpack data(radius(8-bit), extrude(4-bit))
+  float compressed = a_packed_data.z;
+
+  // extrude(4-bit)
+  vec2 extrude;
+  extrude.y = floor(compressed * SHIFT_RIGHT10);
+  compressed -= extrude.y * SHIFT_LEFT10;
+  extrude.y = extrude.y - 1.;
+
+  extrude.x = floor(compressed * SHIFT_RIGHT8);
+  compressed -= extrude.x * SHIFT_LEFT8;
+  extrude.x = extrude.x - 1.;
+
+  // radius(8-bit)
+  float radius = compressed;
+  v_radius = radius;
+
   float zoom_scale = pow(2., 20. - u_zoom);
-  vec2 offset = a_shape * (a_radius + u_stroke_width) * zoom_scale;
+  vec2 offset = extrude * (radius + u_stroke_width) * zoom_scale;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xy + offset, 0.0, 1.0);
 
   // anti-alias
-  float antialiasblur = 1.0 / (a_radius + u_stroke_width);
+  float antialiasblur = 1.0 / (radius + u_stroke_width);
 
   // construct point coords
-  v_data = vec3(a_shape, antialiasblur);
+  v_data = vec3(extrude, antialiasblur);
 
   // picking
-  if(pickingId == u_activeId) {
+  if(picking_id == u_activeId) {
     v_color = u_activeColor;
   }
-  worldId = id_toPickColor(pickingId);
+  worldId = id_toPickColor(picking_id);
 }
