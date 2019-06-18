2214755b1d1c146496fc9166c70eedf7d0bafe52
yuqi.pyq <yuqi.pyq@antfin.com>
2019-06-18 18:23:51 +0800

    feat(point-layer): use 2d sdf functions

--- src/geom/shader/shaderChunks/decode.glsl
+++ src/geom/shader/shaderChunks/decode.glsl
@@ -1,7 +1,20 @@
+#define SHIFT_RIGHT17 1.0 / 131072.0
 #define SHIFT_RIGHT18 1.0 / 262144.0
+#define SHIFT_RIGHT19 1.0 / 524288.0
 #define SHIFT_RIGHT20 1.0 / 1048576.0
+#define SHIFT_RIGHT21 1.0 / 2097152.0
+#define SHIFT_RIGHT22 1.0 / 4194304.0
+#define SHIFT_RIGHT23 1.0 / 8388608.0
+#define SHIFT_RIGHT24 1.0 / 16777216.0
+
+#define SHIFT_LEFT17 131072.0
 #define SHIFT_LEFT18 262144.0
+#define SHIFT_LEFT19 524288.0
 #define SHIFT_LEFT20 1048576.0
+#define SHIFT_LEFT21 2097152.0
+#define SHIFT_LEFT22 4194304.0
+#define SHIFT_LEFT23 8388608.0
+#define SHIFT_LEFT24 16777216.0
 
 vec2 unpack_float(const float packedValue) {
   int packedIntValue = int(packedValue);
