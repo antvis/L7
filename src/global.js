43a3eb6d5cfb94f14ecb479394346a547f4347b9
thinkinggis <lzx199065@gmail.com>
2019-07-02 20:54:49 +0800

    update version

--- src/global.js
+++ src/global.js
@@ -5,7 +5,7 @@
 // const Global = {};
 const FONT_FAMILY = '"-apple-system", BlinkMacSystemFont, "Segoe UI", Roboto,"Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei",SimSun, "sans-serif"';
 const Global = {
-  version: '1.2.0-beta.0',
+  version: '1.2.0-beta.1',
   scene: {
     mapType: 'AMAP',
     zoom: 5,
