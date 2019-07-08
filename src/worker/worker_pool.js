beca89074e374e16111265708785f0adf977eeee
thinkinggis <lzx199065@gmail.com>
2019-07-08 16:07:47 +0800

    feat(worker): worker Source

--- src/worker/worker_pool.js
+++ src/worker/worker_pool.js
@@ -1,54 +1,38 @@
 import WebWorker from './web_worker';
+
+/**
+ * Constructs a worker pool.
+ * @private
+ */
 export default class WorkerPool {
-  constructor(workerCount) {
-    this.workerCount = workerCount || Math.max(Math.floor(window.navigator.hardwareConcurrency / 2), 1);
-    this.workers = []; // worker线程池
-    this.workerQueue = []; // 任务队列
-    this._initWorker(); // 初始化线程池
+
+  constructor() {
+    this.active = {};
   }
-  _initWorker() {
-    while (this.workers.length < this.workerCount) {
-      this.workers.push(new WebWorker());
-    }
-  }
-  runTask(payload) {
-    return new Promise((resolve, reject) => {
-      if (this.workers.length > 0) {
-        const worker = this.workers.shift(); // 从线程池取出一个worker
-        worker.postMessage(payload); // 向线程发送数据
-        const workerCallback = e => {
-          resolve(e.data); // 成功则返回数据
-          // 移除事件监听
-          worker.removeEventListener('message', workerCallback);
-          // 重新放回线程池
-          this.workers.push(worker);
-          // 如果任务队列的数据还有则从任务队列继续取数据执行任务
-          if (this.workerQueue.length > 0) {
-            const queueData = this.workerQueue.shift();
-            this.runTask(queueData.payload).then(data => {
-              queueData.resolve(data);
-            });
-          }
-        };
-        // 监听worker事件
-        worker.addEventListener('message', workerCallback);
-        worker.addEventListener('error', e => {
-          reject('filename:' + e.filename + '\nmessage:' + e.message + '\nlineno:' + e.lineno);
-        });
-      } else {
-        // 如果线程池都被占用，则将数据丢入任务队列，并保存对应的resolve和reject
-        this.workerQueue.push({
-          payload,
-          resolve,
-          reject
-        });
+
+  acquire(mapId) {
+    if (!this.workers) {
+      // Lazily look up the value of mapboxgl.workerCount so that
+      // client code has had a chance to set it.
+      this.workers = [];
+      while (this.workers.length < WorkerPool.workerCount) {
+        this.workers.push(new WebWorker());
       }
-    });
+    }
+
+    this.active[mapId] = true;
+    return this.workers.slice();
   }
-  release() {
-    this.workers.forEach(worker => {
-      worker.terminate();
-    });
+
+  release(mapId) {
+    delete this.active[mapId];
+    if (Object.keys(this.active).length === 0) {
+      this.workers.forEach(w => {
+        w.terminate();
+      });
+      this.workers = null;
+    }
   }
 }
 
+WorkerPool.workerCount = Math.max(Math.floor(window.navigator.hardwareConcurrency / 2), 1);
