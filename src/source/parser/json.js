4b4e33dc9f51d1184bdd612b66d12bfcd30d36d4
thinkinggis <lzx199065@gmail.com>
2019-07-10 11:18:40 +0800

    fix(poylygon): json source

--- src/source/parser/json.js
+++ src/source/parser/json.js
@@ -1,16 +1,21 @@
 export default function json(data, cfg) {
-  const { x, y, x1, y1 } = cfg;
+  const { x, y, x1, y1, coordinates } = cfg;
   const resultdata = [];
   data.forEach((col, featureIndex) => {
-    let coordinates = [];
-    if (x && y) { coordinates = [ col[x], col[y] ]; } // 点数据
+    let coords = [];
+    if (x && y) { coords = [ col[x], col[y] ]; } // 点数据
     if (x1 && y1) { // 弧线 或者线段
-      coordinates = [[ col[x], col[y] ], [ col[x1], col[y1] ]];
+      coords = [[ col[x], col[y] ], [ col[x1], col[y1] ]];
     }
+    if (coordinates) {
+      coords = col[coordinates];
+      delete col[coordinates];
+    }
+
     col._id = featureIndex + 1;
     const dataItem = {
       ...col,
-      coordinates
+      coordinates: coords
 
     };
     resultdata.push(dataItem);
