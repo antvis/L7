ebc892607a4a393dc406c095cc2f98939b346d21
izzy.cz <izzy.cz@antfin.com>
2019-07-12 20:38:20 +0800

    线支持pattern

--- src/geom/shader/meshline_vert.glsl
+++ src/geom/shader/meshline_vert.glsl
@@ -3,6 +3,7 @@ attribute vec4 a_color;
 attribute float a_size;
 attribute float a_distance;
 attribute float a_dash_array;
+attribute float a_total_distance;
 
 uniform float u_zoom;
 uniform float u_time : 0;
@@ -11,9 +12,11 @@ uniform vec4 u_activeColor : [ 1.0, 0, 0, 1.0 ];
 
 varying float v_time;
 varying vec4 v_color;
-varying float v_distance;
 varying float v_dash_array;
 varying vec2 v_normal;
+#ifdef DASHLINE
+varying float v_distance_ratio;
+#endif
 
 #ifdef ANIMATE 
 uniform float u_duration : 2.0;
@@ -21,22 +24,41 @@ uniform float u_interval : 1.0;
 uniform float u_trailLength : 0.2;
 #endif
 
+#ifdef TEXTURE
+attribute vec2 a_texture_coord;
+uniform float u_pattern_spacing : 0.0;
+varying vec2 v_uv;
+varying float v_texture_y;
+varying float v_texture_percent;
+#endif
 void main() {
+  // extrude along normal
+  float extrude_scale = pow(2.0, 20.0 - u_zoom);
   v_color = a_color;
-  v_distance = a_distance;
   v_dash_array = a_dash_array;
+  float distance_ratio = a_distance / a_total_distance;
+  #ifdef DASHLINE
+    v_distance_ratio = distance_ratio;
+  #endif
+  #ifdef TEXTURE
+    v_uv = vec2(a_texture_coord.x + 0.125, a_texture_coord.y);
+    if (a_miter > 0.0) {
+      v_uv.x = a_texture_coord.x;
+    }
+    v_texture_y = a_distance / extrude_scale / (a_size + u_pattern_spacing);
+    v_texture_percent = a_size / (a_size + u_pattern_spacing);
+  #endif
 
   // anti-alias
   v_normal = vec2(normal * sign(a_miter));
 
-  // extrude along normal
-  float extrude_scale = pow(2.0, 20.0 - u_zoom);
+  
   vec3 offset = vec3(normal * a_size * extrude_scale / 2.0 * a_miter);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xy + offset.xy, 0., 1.0);
   // gl_Position.z -=0.8 * gl_Position.w;
 
   #ifdef ANIMATE
-    float alpha =1.0 - fract( mod(1.0- a_distance,u_interval)* (1.0/u_interval) + u_time / u_duration);
+    float alpha =1.0 - fract( mod(1.0- distance_ratio,u_interval)* (1.0/u_interval) + u_time / u_duration);
     alpha = (alpha + u_trailLength -1.0) / u_trailLength;
     v_time = clamp(alpha,0.,1.);
   #endif
