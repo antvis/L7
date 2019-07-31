de41c7e45a60455baf8a3ac3eb04daf14a01e301
thinkinggis <lzx199065@gmail.com>
2019-07-31 11:52:52 +0800

    refactor(heatmap) buffer

--- src/geom/buffer/heatmap/hexagon_3d.js
+++ src/geom/buffer/heatmap/hexagon_3d.js
@@ -1,33 +1,40 @@
+/**
+ * instantcebufferGeometry的组装方式
+ */
 import BufferBase from '../buffer';
-import { fill, extrude } from '../../shape/polygon';
 import { fillPolygon, extrude_Polygon } from '../../extrude';
-import { polygonPath } from '../../shape/path';
-export default class Grid3D extends BufferBase {
+import Global from '../../../global';
+import * as shapePath from '../../shape/path';
+
+export default class Hexagon3D extends BufferBase {
   _buildFeatures() {
     const layerData = this.get('layerData');
     this._offset = 0;
 
     layerData.forEach(feature => {
-
+      this._calculateFill(feature);
     });
   }
-  _initAttributes() {
-    super._initAttributes();
-    this.attributes.miters = new Float32Array(this.verticesCount * 3);
-    this.attributes.normals = new Float32Array(this.verticesCount * 3);
-  }
   _calculateFeatures() {
-    const hexgonPoints = polygonPath(6);
-    const hexgonFill = fillPolygon([ hexgonPoints ]);
+    const shape = this.get('shapeType');
+    const hexgonFill = this.getShape(shape);
     const layerData = this.get('layerData');
-    this.verticesCount = hexgonFill.positions.length / 3 * layerData.length;
-    this.indexCount = hexgonFill.indexArray * layerData.length;
-    this.featureBuffer = hexgonFill;
+    this.verticesCount = layerData.length;
+    this.indexCount = 0;
+    this.instanceGeometry = hexgonFill;
   }
-  _calculatefill(feature) {
-    // this.
+  _calculateFill(feature) {
+
+    feature.bufferInfo = { verticesOffset: this._offset };
+    const { coordinates } = feature;
+    this._encodeArray(feature, 1);
+    this.attributes.positions.set(coordinates, this._offset * 3);
+    this._offset++;
   }
-  _getPoints(num) {
-    return polygonPath(num);
+  getShape(shape) {
+    const { pointShape } = Global;
+    if (pointShape['3d'].indexOf(shape) !== -1) return extrude_Polygon([ shapePath[shape]() ]);
+    if (pointShape['2d'].indexOf(shape) !== -1) return fillPolygon([ shapePath[shape]() ]);
+    return fillPolygon([ shapePath[shape]() ]);
   }
 }
