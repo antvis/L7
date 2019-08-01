7a255a81a797aba12ecedc275fb90a4b545e1fdd
thinkinggis <lzx199065@gmail.com>
2019-08-01 19:25:32 +0800

    update version

--- src/core/scene.js
+++ src/core/scene.js
@@ -27,8 +27,8 @@ export default class Scene extends Base {
 
   _initEngine(mapContainer) {
     this._engine = new Engine(mapContainer, this);
-    // this.registerMapEvent();
-    this._engine.run();
+    this.registerMapEvent();
+    // this._engine.run();
     compileBuiltinModules();
   }
   // 为pickup场景添加 object 对象
