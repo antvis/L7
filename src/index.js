e384f1cb48a7563eaf6d724ecc71beef01a4964c
thinkinggis <lzx199065@gmail.com>
2019-08-07 10:44:29 +0800

    feat(control): add scale, layer.zoom control

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './component/css/l7.css';
 import Scene from './core/scene';
 import Global from './global';
 import Source from './core/source';
@@ -5,6 +6,8 @@ import TileSource from './source/tile_source';
 import { registerParser, registerTransform } from './source';
 import { registerInteraction, getInteraction } from './interaction';
 import { registerLayer } from './layer';
+import Popup from './component/popup';
+import * as Control from './component/control';
 const version = Global.version;
 const exported = {
   version,
@@ -15,6 +18,8 @@ const exported = {
   registerTransform,
   registerLayer,
   registerInteraction,
-  getInteraction
+  getInteraction,
+  Popup,
+  Control
 };
 export default exported;
