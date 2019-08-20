f439958c237a9194090ffe26766e5fe1c3fc3a9b
thinkinggis <lzx199065@gmail.com>
2019-08-20 10:32:46 +0800

    fix(point): 修复shape映射,图片加载的事件

--- src/geom/shader/arcline_frag.glsl
+++ src/geom/shader/arcline_frag.glsl
@@ -1,6 +1,18 @@
   precision mediump float;
   uniform float u_opacity;
   varying vec4 v_color;
+  uniform float u_time : 0;
+
+  #if defined DASHLINE  || defined ANIMATE
+    varying float v_distance_ratio;
+  #endif
+
+  #ifdef ANIMATE 
+  uniform float u_duration : 2.0;
+  uniform float u_interval : 1.0;
+  uniform float u_trailLength : 0.2;
+  #endif
+
   
   void main() {
       if(v_color.a == 0.){
@@ -9,5 +21,13 @@
    
       gl_FragColor = v_color;
       gl_FragColor.a = v_color.a*u_opacity;
+      #ifdef ANIMATE
+        float alpha =1.0 - fract( mod(1.0- v_distance_ratio, u_interval)* (1.0/u_interval) + u_time / u_duration);
+          // alpha = (alpha + u_trailLength -1.0) / u_trailLength;
+          // alpha = smoothstep(0., 1., alpha);
+        gl_FragColor.a *= alpha;
+   
+      #endif
+
       #pragma include "pick"
   }
