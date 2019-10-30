dd97c55ac49257e36bff73c75bf3114b2db79c8e
thinkinggis <lzx199065@gmail.com>
2019-10-30 14:53:18 +0800

    feat(layers): add arclayer

--- packages/layers/src/core/triangulation.ts
+++ packages/layers/src/core/triangulation.ts
@@ -137,6 +137,44 @@ export function RasterImageTriangulation(feature: IEncodeFeature) {
   };
 }
 
+export function LineArcTriangulation(feature: IEncodeFeature, segNum = 30) {
+  const coordinates = feature.coordinates as IPosition[];
+  const positions = [];
+  const indexArray = [];
+  for (let i = 0; i < segNum; i++) {
+    // 上线两个顶点
+    // [ x, y, z, sx,sy, tx,ty]
+    positions.push(
+      i,
+      1,
+      i,
+      coordinates[0][0],
+      coordinates[0][1],
+      coordinates[1][0],
+      coordinates[1][1],
+      i,
+      -1,
+      i,
+      coordinates[0][0],
+      coordinates[0][1],
+      coordinates[1][0],
+      coordinates[1][1],
+    );
+    if (i !== segNum - 1) {
+      indexArray.push(
+        ...[0, 1, 2, 1, 3, 2].map((v) => {
+          return i * 2 + v;
+        }),
+      );
+    }
+  }
+  return {
+    vertices: positions,
+    indices: indexArray,
+    size: 7,
+  };
+}
+
 /**
  * 点图层3d geomerty
  * @param shape 3D形状
--- packages/layers/src/polygon/shaders/polygon_vert.glsl
+++ packages/layers/src/polygon/shaders/polygon_vert.glsl
@@ -11,7 +11,7 @@ varying vec4 v_Color;
 
 void main() {
   v_Color =a_Color;
-  vec4 project_pos = project_position(vec4(a_Position 1.0));
+  vec4 project_pos = project_position(vec4(a_Position,1.0));
   gl_Position = project_common_position_to_clipspace(vec4(project_pos.xyz, 1.0));
 
   setPickingColor(a_PickingColor);
