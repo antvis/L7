2e37701e1d87cf542d0de63223c09e8acaee2ca9
yuqi.pyq <yuqi.pyq@antfin.com>
2019-11-01 17:15:20 +0800

    ci: init travis config

--- packages/core/src/shaders/lighting.glsl
+++ packages/core/src/shaders/lighting.glsl
@@ -1,11 +1,11 @@
 // Blinn-Phong model
 // apply lighting in vertex shader instead of fragment shader
 // @see https://learnopengl.com/Advanced-Lighting/Advanced-Lighting
-uniform float u_ambient : 1.0;
-uniform float u_diffuse : 1.0;
-uniform float u_specular : 1.0;
-uniform int u_num_of_directional_lights : 1;
-uniform int u_num_of_spot_lights : 0;
+uniform float u_Ambient : 1.0;
+uniform float u_Diffuse : 1.0;
+uniform float u_Specular : 1.0;
+uniform int u_NumOfDirectionalLights : 1;
+uniform int u_NumOfSpotLights : 0;
 
 #define SHININESS 32.0
 #define MAX_NUM_OF_DIRECTIONAL_LIGHTS 3
@@ -32,8 +32,8 @@ struct SpotLight {
   float exponent;
 };
 
-uniform DirectionalLight u_directional_lights[MAX_NUM_OF_DIRECTIONAL_LIGHTS];
-uniform SpotLight u_spot_lights[MAX_NUM_OF_SPOT_LIGHTS];
+uniform DirectionalLight u_DirectionalLights[MAX_NUM_OF_DIRECTIONAL_LIGHTS];
+uniform SpotLight u_SpotLights[MAX_NUM_OF_SPOT_LIGHTS];
 
 vec3 calc_directional_light(DirectionalLight light, vec3 normal, vec3 viewDir) {
   vec3 lightDir = normalize(light.direction);
@@ -43,9 +43,9 @@ vec3 calc_directional_light(DirectionalLight light, vec3 normal, vec3 viewDir) {
   vec3 halfwayDir = normalize(lightDir + viewDir);
   float spec = pow(max(dot(normal, halfwayDir), 0.0), SHININESS);
 
-  vec3 ambient = light.ambient * u_ambient;
-  vec3 diffuse = light.diffuse * diff * u_diffuse;
-  vec3 specular = light.specular * spec * u_specular;
+  vec3 ambient = light.ambient * u_Ambient;
+  vec3 diffuse = light.diffuse * diff * u_Diffuse;
+  vec3 specular = light.specular * spec * u_Specular;
 
   return ambient + diffuse + specular;
 }
@@ -62,9 +62,9 @@ vec3 calc_directional_light(DirectionalLight light, vec3 normal, vec3 viewDir) {
 //   float attenuation = 1.0 / (light.constant + light.linear * distance + 
 //           light.quadratic * (distance * distance));    
 
-//   vec3 ambient = light.ambient * u_ambient;
-//   vec3 diffuse = light.diffuse * diff * u_diffuse;
-//   vec3 specular = light.specular * spec * u_specular;
+//   vec3 ambient = light.ambient * u_Ambient;
+//   vec3 diffuse = light.diffuse * diff * u_Diffuse;
+//   vec3 specular = light.specular * spec * u_Specular;
 
 //   float spotEffect = dot(normalize(light.direction), -lightDir);
 //   float spotCosCutoff = cos(light.angle / 180.0 * PI);
@@ -82,16 +82,16 @@ vec3 calc_directional_light(DirectionalLight light, vec3 normal, vec3 viewDir) {
 vec3 calc_lighting(vec3 position, vec3 normal, vec3 viewDir) {
   vec3 weight = vec3(0.0);
   for (int i = 0; i < MAX_NUM_OF_DIRECTIONAL_LIGHTS; i++) {
-    if (i >= u_num_of_directional_lights) {
+    if (i >= u_NumOfDirectionalLights) {
       break;
     }
-    weight += calc_directional_light(u_directional_lights[i], normal, viewDir);
+    weight += calc_directional_light(u_DirectionalLights[i], normal, viewDir);
   }
   // for (int i = 0; i < MAX_NUM_OF_SPOT_LIGHTS; i++) {
-  //   if (i >= u_num_of_spot_lights) {
+  //   if (i >= u_NumOfSpotLights) {
   //     break;
   //   }
-  //   weight += calc_spot_light(u_spot_lights[i], normal, position, viewDir);
+  //   weight += calc_spot_light(u_SpotLights[i], normal, position, viewDir);
   // }
   return weight;
 }
