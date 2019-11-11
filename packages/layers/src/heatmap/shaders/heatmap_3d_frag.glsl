7d106d19fa8ea6e7177fed71f1f57efef1dd282f
thinkinggis <lzx199065@gmail.com>
2019-11-11 13:52:00 +0800

    fix(layers): heatmap 3d effect

--- packages/layers/src/heatmap/shaders/heatmap_3d_frag.glsl
+++ packages/layers/src/heatmap/shaders/heatmap_3d_frag.glsl
@@ -5,14 +5,13 @@ varying vec2 v_texCoord;
 varying float v_intensity;
 
 void main(){
-    float intensity = texture2D(u_texture, v_texCoord).r;
+   
+     float intensity = texture2D(u_texture, v_texCoord).r;
      vec2 ramp_pos = vec2(
         fract(16.0 * (1.0 - v_intensity)),
         floor(16.0 * (1.0 - v_intensity)) / 16.0);
     // vec4 color = texture2D(u_colorTexture,vec2(0.5,1.0-intensity));
     vec4 color = texture2D(u_colorTexture,ramp_pos);
     gl_FragColor = color;
-    // gl_FragColor.a = color.a * smoothstep(0.0, 0.01, v_intensity) * u_opacity;
-    // gl_FragColor.a = 0.2;
-
+    gl_FragColor.a = color.a * smoothstep(0.1,0.2,intensity)* u_opacity;
 }
