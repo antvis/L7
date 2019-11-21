956d24feb038a9d4045bcbbc0a89071092e60869
thinkinggis <lzx199065@gmail.com>
2019-11-21 20:15:06 +0800

    docs(site): update site

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -4,9 +4,9 @@ import Cases from '@antv/gatsby-theme-antv/site/components/Cases';
 import Companies from '@antv/gatsby-theme-antv/site/components/Companies';
 import Features from '@antv/gatsby-theme-antv/site/components/Features';
 import SEO from '@antv/gatsby-theme-antv/site/components/Seo';
-import '../css/home.css';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import '../css/home.css';
 
 const IndexPage = () => {
   const { t, i18n } = useTranslation();
@@ -34,14 +34,46 @@ const IndexPage = () => {
     },
   ];
   const companies = [
-    { name: '阿里云', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*V_xMRIvw2iwAAAAAAAAAAABkARQnAQ' },
-    { name: '支付宝', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*lYDrRZvcvD4AAAAAAAAAAABkARQnAQ', },
-    { name: '天猫', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*BQrxRK6oemMAAAAAAAAAAABkARQnAQ', },
-    { name: '淘宝网', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*1l8-TqUr7UcAAAAAAAAAAABkARQnAQ', },
-    { name: '网商银行', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*ZAKFQJ5Bz4MAAAAAAAAAAABkARQnAQ', },
-    { name: '盒马',img: 'https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*ePJMQZCb8vkAAAAAAAAAAABkARQnAQ', },
-    { name: 'yunos', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*_js7SaNosUwAAAAAAAAAAABkARQnAQ', },
-    { name: '菜鸟', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*TgV-RZDODJIAAAAAAAAAAABkARQnAQ', },
+    {
+      name: '阿里云',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*V_xMRIvw2iwAAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: '支付宝',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*lYDrRZvcvD4AAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: '天猫',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*BQrxRK6oemMAAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: '淘宝网',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*1l8-TqUr7UcAAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: '网商银行',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*ZAKFQJ5Bz4MAAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: '盒马',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*ePJMQZCb8vkAAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: 'yunos',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*_js7SaNosUwAAAAAAAAAAABkARQnAQ',
+    },
+    {
+      name: '菜鸟',
+      img:
+        'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*TgV-RZDODJIAAAAAAAAAAABkARQnAQ',
+    },
   ];
   const bannerButtons = [
     {
@@ -66,21 +98,27 @@ const IndexPage = () => {
 
   const cases = [
     {
-      logo:'https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*gjBmT56SDgsAAAAAAAAAAABkARQnAQ',
-      title: t('浅色色板'),
-      description: t('一个个真实的地理数据可视化案例，将复杂的地理数据,通过简单，易用的API接口,让用户达到开箱即用的效果。'),
+      logo:
+        'https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*gjBmT56SDgsAAAAAAAAAAABkARQnAQ',
+      title: t('浅版精彩案例'),
+      description: t(
+        '一个个真实的地理数据可视化案例，将复杂的地理数据,通过简单，易用的API接口,让用户达到开箱即用的效果。',
+      ),
       link: `/${i18n.language}/examples/gallery/basic`,
       image:
         'https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*RPxeQZ8Uk7EAAAAAAAAAAABkARQnAQ',
     },
     {
-      logo:'https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*gjBmT56SDgsAAAAAAAAAAABkARQnAQ',
-      title: t('深色色板'),
-      description: t('一个个真实的地理数据可视化案例，将复杂的地理数据,通过简单，易用的API接口,让用户达到开箱即用的效果。'),
+      logo:
+        'https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*gjBmT56SDgsAAAAAAAAAAABkARQnAQ',
+      title: t('深版精彩案例'),
+      description: t(
+        '一个个真实的地理数据可视化案例，将复杂的地理数据,通过简单，易用的API接口,让用户达到开箱即用的效果。',
+      ),
       link: `/${i18n.language}/examples/gallery/basic`,
       image:
         'https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*B8rtTpvkqTgAAAAAAAAAAABkARQnAQ',
-    }
+    },
   ];
 
   return (
@@ -89,12 +127,11 @@ const IndexPage = () => {
       <Banner
         coverImage={
           <img
-            className="cover-image"
-<<<<<<< HEAD
-            src="https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*cCI7RaJs46AAAAAAAAAAAABkARQnAQ"
-=======
+            width="100%"
+            class="Notification-module--number--31-3Z"
+            style={{ marginLeft: '125px', marginTop: '50px' }}
             src="https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*VcojS5aWhMYAAAAAAAAAAABkARQnAQ"
->>>>>>> f40e44f... fix(fix): fix
+>
           />
         }
         title={t('L7 地理空间数据可视分析引擎')}
@@ -108,12 +145,9 @@ const IndexPage = () => {
       />
       <Features features={features} style={{ width: '100%' }} />
       <Cases style={{ width: '102%' }} cases={cases} />
-      <Companies
-        title={t('感谢信赖')}
-        companies={companies}
-      />
+      <Companies title={t('感谢信赖')} companies={companies} />
     </>
   );
-};
+}
 
-export default IndexPage;
+export default IndexPage
