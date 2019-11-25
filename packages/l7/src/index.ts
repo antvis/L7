9023c05ff6d66f92a9a7574c62da3f0a4893cf10
thinkinggis <lzx199065@gmail.com>
2019-11-25 13:39:50 +0800

    fix(l7): modules import

--- packages/l7/src/index.ts
+++ packages/l7/src/index.ts
@@ -1,8 +1,8 @@
-
-// export * from '@l7/core';
-// // @ts-ignore
-// export * from '@l7/scene';
-// // @ts-ignore
-// export * from '@l7/layers';
-// // @ts-ignore
-// export * from '@l7/component';
+// @ts-ignore
+export * from '@l7/core';
+// @ts-ignore
+export * from '@l7/scene';
+// @ts-ignore
+export * from '@l7/layers';
+// @ts-ignore
+export * from '@l7/component';
