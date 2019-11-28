d2d4569389bda13dbf8742403e9ed7dfa6ea669e
thinkinggis <lzx199065@gmail.com>
2019-11-28 23:38:18 +0800

    docs(docs): fix docs error

--- packages/layers/src/point/shaders/extrude_vert.glsl
+++ packages/layers/src/point/shaders/extrude_vert.glsl
@@ -23,7 +23,7 @@ void main() {
   vec2 offset = project_pixel(size.xy);
 
   vec4 project_pos = project_position(vec4(a_Pos.xy, 0, 1.0));
-  vec4 pos = vec4(project_pos.xy + offset, size.z, 1.0);
+  vec4 pos = vec4(project_pos.xy + offset, project_pixel(size.z), 1.0);
 
   float lightWeight = calc_lighting(pos);
   v_color =vec4(a_Color.rgb * lightWeight, a_Color.w);
