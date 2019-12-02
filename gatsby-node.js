1c2d35facb2fe60bbde4985bc6a0ddb5a21e3f04
thinkinggis <lzx199065@gmail.com>
2019-12-02 17:55:16 +0800

    fix(gatsby): gastsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,13 @@ exports.onCreateWebpackConfig = ({ getConfig }) => {
   config.resolve.alias = {
     ...config.resolve.alias,
     '@antv/l7': path.resolve(__dirname, 'packages/l7/src'),
-    '@antv/l7-maps': path.resolve(__dirname, 'packages/maps/src')
+    '@antv/l7-core': path.resolve(__dirname, 'packages/core/src'),
+    '@antv/l7-component': path.resolve(__dirname, 'packages/component/src'),
+    '@antv/l7-layers': path.resolve(__dirname, 'packages/layers/src'),
+    '@antv/l7-maps': path.resolve(__dirname, 'packages/maps/src'),
+    '@antv/l7-renderer': path.resolve(__dirname, 'packages/renderer/src'),
+    '@antv/l7-scene': path.resolve(__dirname, 'packages/scene/src'),
+    '@antv/l7-source': path.resolve(__dirname, 'packages/source/src'),
+    '@antv/l7-utils': path.resolve(__dirname, 'packages/utils/src'),
   };
 };
