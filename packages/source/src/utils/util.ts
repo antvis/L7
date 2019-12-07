ea975c9daf4bf76c9723ec2081bdd128539dff66
thinkinggis <lzx199065@gmail.com>
2019-12-07 20:25:52 +0800

    fix(tslint): igonre lint test data lint

--- packages/source/src/utils/util.ts
+++ packages/source/src/utils/util.ts
@@ -1,7 +1,6 @@
 interface IDataItem {
   [key: string]: any;
-};
-
+}
 export function getColumn(data: IDataItem[], columnName: string) {
   return data.map((item: IDataItem) => {
     return item[columnName] * 1;
