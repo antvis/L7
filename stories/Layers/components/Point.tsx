85f9fae68d78c2981be9a8cb3fefbd4119e00808
thinkinggis <lzx199065@gmail.com>
2019-12-18 14:32:11 +0800

    docs: add layer docs

--- stories/Layers/components/Point.tsx
+++ stories/Layers/components/Point.tsx
@@ -44,7 +44,33 @@ export default class Point3D extends React.Component {
       scene.addLayer(pointLayer);
       pointLayer.on('mousemove', (e) => {
         const id = e.featureId;
-        console.log(id);
+        console.log(e.type);
+        pointLayer.setActive(id);
+      });
+      pointLayer.on('mousedown', (e) => {
+        const id = e.featureId;
+        console.log(e.type);
+        pointLayer.setActive(id);
+      });
+      pointLayer.on('mouseup', (e) => {
+        const id = e.featureId;
+        console.log(e.type);
+        pointLayer.setActive(id);
+      });
+      pointLayer.on('click', (e) => {
+        const id = e.featureId;
+        console.log(e.type);
+        pointLayer.setActive(id);
+      });
+
+      pointLayer.on('contextmenu', (e) => {
+        const id = e.featureId;
+        console.log(e.type);
+        pointLayer.setActive(id);
+      });
+      pointLayer.on('unpick', (e) => {
+        const id = e.featureId;
+        console.log(e.type);
         pointLayer.setActive(id);
       });
 
