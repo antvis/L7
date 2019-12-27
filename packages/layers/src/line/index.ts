4a30d0364ea407736755e50f8d04960bf56f7b86
thinkinggis <lzx199065@gmail.com>
2019-12-27 23:16:54 +0800

    fix: animate time

--- packages/layers/src/line/index.ts
+++ packages/layers/src/line/index.ts
@@ -5,8 +5,6 @@ import LineModels, { LineModelType } from './models';
 export default class LineLayer extends BaseLayer<ILineLayerStyleOptions> {
   public type: string = 'LineLayer';
 
-  private animateStartTime: number = 0;
-
   protected getConfigSchema() {
     return {
       properties: {
@@ -48,13 +46,5 @@ export default class LineLayer extends BaseLayer<ILineLayerStyleOptions> {
     );
     const shape = shapeAttribute?.scale?.field as LineModelType;
     return shape || 'line';
-  }
-  // 拆分的动画插件
-  private initAnimate() {
-    const { enable } = this.animateOptions;
-    if (enable) {
-      this.layerService.startAnimate();
-      this.animateStartTime = this.layerService.clock.getElapsedTime();
-    }
   }
 }
