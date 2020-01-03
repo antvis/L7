1bb88119191f97ff23fcc5c6da6e010db9d99a5c
thinkinggis <lzx199065@gmail.com>
2020-01-03 14:43:51 +0800

    feat: marker 支持鼠标事件

--- examples/raster/basic/demo/light.js
+++ examples/raster/basic/demo/light.js
@@ -1,3 +1,4 @@
+// https://gw.alipayobjects.com/zos/antvdemo/assets/2019_clip/ndvi_201905.tif
 import { RasterLayer, Scene } from '@antv/l7';
 import { GaodeMap } from '@antv/l7-maps';
 import * as GeoTIFF from 'geotiff';
@@ -13,7 +14,7 @@ const scene = new Scene({
 addLayer();
 async function getTiffData() {
   const response = await fetch(
-    'https://gw.alipayobjects.com/os/rmsportal/XKgkjjGaAzRyKupCBiYW.dat'
+    'https://gw.alipayobjects.com/zos/antvdemo/assets/light_clip/lightF141999.tiff'
   );
   const arrayBuffer = await response.arrayBuffer();
   const tiff = await GeoTIFF.fromArrayBuffer(arrayBuffer);
@@ -24,9 +25,7 @@ async function getTiffData() {
   return {
     data: values[0],
     width,
-    height,
-    min: 0,
-    max: 8000
+    height
   };
 }
 
@@ -40,16 +39,18 @@ async function addLayer() {
         type: 'raster',
         width: tiffdata.width,
         height: tiffdata.height,
-        extent: [ 73.482190241, 3.82501784112, 135.106618732, 57.6300459963 ]
+        extent: [ 73.4821902409999979, 3.8150178409999995, 135.1066187319999869, 57.6300459959999998 ]
       }
     })
     .style({
-      heightRatio: 100,
-      opacity: 0.8,
-      domain: [ 0, 8000 ],
+      opacity: 1.0,
+      clampLow: false,
+      clampHigh: false,
+      domain: [ 0, 90 ],
+      nodataValue: 0,
       rampColors: {
-        colors: [ '#FF4818', '#F7B74A', '#FFF598', '#91EABC', '#2EA9A1', '#206C7C' ].reverse(),
-        positions: [ 0, 0.2, 0.4, 0.6, 0.8, 1.0 ]
+        colors: [ 'rgb(166,97,26)', 'rgb(223,194,125)', 'rgb(245,245,245)', 'rgb(128,205,193)', 'rgb(1,133,113)' ],
+        positions: [ 0, 0.25, 0.5, 0.75, 1.0 ]
       }
     });
 
