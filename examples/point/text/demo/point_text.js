eb0dac4991cb2cdd181a754360e0ef851cd8f986
thinkinggis <lzx199065@gmail.com>
2020-01-07 13:54:30 +0800

    docs: add animate demo

--- examples/point/text/demo/point_text.js
+++ examples/point/text/demo/point_text.js
@@ -23,15 +23,15 @@ fetch('https://gw.alipayobjects.com/os/rmsportal/oVTMqfzuuRFKiDwhPSFL.json')
         }
       })
       .shape('m', 'text')
-      .size(18)
-      .color('w', [ '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac' ])
+      .size(12)
+      .color('w', [ '#0e0030', '#0e0030', '#0e0030' ])
       .style({
         textAnchor: 'center', // 文本相对锚点的位置 center|left|right|top|bottom|top-left
         textOffset: [ 0, 0 ], // 文本相对锚点的偏移量 [水平, 垂直]
         spacing: 2, // 字符间距
         padding: [ 1, 1 ], // 文本包围盒 padding [水平，垂直]，影响碰撞检测结果，避免相邻文本靠的太近
-        stroke: 'red', // 描边颜色
-        strokeWidth: 1, // 描边宽度
+        stroke: '#ffffff', // 描边颜色
+        strokeWidth: 0.3, // 描边宽度
         strokeOpacity: 1.0
       });
 
