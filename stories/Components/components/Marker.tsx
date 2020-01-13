ecda21c0db7e924ea02af03e4c752527074a5890
thinkinggis <lzx199065@gmail.com>
2020-01-13 20:57:55 +0800

    chore: 测试用例jest-electron 环境 & 修复一些文档问题

--- stories/Components/components/Marker.tsx
+++ stories/Components/components/Marker.tsx
@@ -1,6 +1,6 @@
 // @ts-ignore
-import { Marker, PolygonLayer, Scene } from '@antv/l7';
-import { Mapbox, GaodeMap } from '@antv/l7-maps';
+import { Marker, PointLayer, PolygonLayer, Scene } from '@antv/l7';
+import { GaodeMap, Mapbox } from '@antv/l7-maps';
 import * as React from 'react';
 
 export default class MarkerComponent extends React.Component {
@@ -19,55 +19,62 @@ export default class MarkerComponent extends React.Component {
       id: 'map',
       map: new GaodeMap({
         style: 'dark',
-        center: [110.19382669582967, 30.258134],
+        center: [120.184824, 30.248341],
         pitch: 0,
-        zoom: 3,
+        zoom: 18,
       }),
     });
     this.scene = scene;
-    const layer = new PolygonLayer({});
 
-    layer
-      .source(data)
-      .size('name', [0, 10000, 50000, 30000, 100000])
-      .active(true)
-      .color('name', [
-        '#2E8AE6',
-        '#69D1AB',
-        '#DAF291',
-        '#FFD591',
-        '#FF7A45',
-        '#CF1D49',
-      ])
-      .shape('fill')
-      .style({
-        opacity: 0.8,
-      });
-    scene.addLayer(layer);
     const marker = new Marker().setLnglat({
-      lng: 120.19382669582967,
-      lat: 30.258134,
-    });
-    marker.on('click', (e) => {
-      console.log(e);
+      lng: 120.184824,
+      lat: 30.248341,
     });
 
+    const arr = [
+      {
+        lng: 120.184824,
+        lat: 30.248341,
+        count: 40,
+      },
+    ];
+    const pointLayer = new PointLayer({})
+      .source(arr, {
+        parser: {
+          type: 'json',
+          x: 'lng',
+          y: 'lat',
+        },
+      })
+      .shape('dot')
+      .active(true)
+      .animate(false)
+      .size(5)
+      .color('#ffa842')
+      .style({
+        opacity: 1,
+      });
+    scene.addLayer(pointLayer);
     scene.addMarker(marker);
+
     scene.on('loaded', () => {
       // @ts-ignore
-      const marker1 = new AMap.Marker({
-        map: scene.map,
-        position: [113.800646, 34.796227],
-        shadow: '#000',
-        label: {
-          content: '站点',
-          direction: 'top',
-        },
+      marker.on('click', (e) => {
+        console.log(this.scene.getZoom());
       });
-      marker1.on('click', () => {
-        alert(1111);
-        console.log('选中的点', 1111);
-      });
+      // const marker1 = new AMap.Marker({
+      //   map: scene.map,
+      //   position: [120.184824, 30.248341],
+      //   shadow: '#000',
+      //   label: {
+      //     content: '站点',
+      //     direction: 'top',
+      //   },
+      // });
+      // marker1.on('click', () => {
+      //  console.log(this.scene.getZoom());
+      //   console.log('选中的点', 1111);
+      // });
     });
   }
 
