4053bdf2af93e638b7311e10a898535b1279da28
thinkinggis <lzx199065@gmail.com>
2020-01-13 20:57:55 +0800

    chore: 测试用例jest-electron 环境 & 修复一些文档问题

--- packages/core/src/shaders/projection.glsl
+++ packages/core/src/shaders/projection.glsl
@@ -98,7 +98,6 @@ vec4 project_position(vec4 position) {
 
   // TODO: 瓦片坐标系 & 常规世界坐标系
 }
-
 vec2 project_pixel_size_to_clipspace(vec2 pixels) {
   vec2 offset = pixels / u_ViewportSize * u_DevicePixelRatio * 2.0;
   return offset * u_FocalDistance;
@@ -107,14 +106,14 @@ vec2 project_pixel_size_to_clipspace(vec2 pixels) {
 float project_pixel(float pixel) {
   if (u_CoordinateSystem == COORDINATE_SYSTEM_P20) {
     // P20 坐标系下，为了和 Web 墨卡托坐标系统一，zoom 默认减1
-    return pixel * pow(2.0, (19.0 - u_Zoom));
+    return pixel * pow(2.0, (20.0 - u_Zoom));
   }
   return pixel;
 }
 vec2 project_pixel(vec2 pixel) {
   if (u_CoordinateSystem == COORDINATE_SYSTEM_P20) {
     // P20 坐标系下，为了和 Web 墨卡托坐标系统一，zoom 默认减1
-    return pixel * pow(2.0, (19.0 - u_Zoom));
+    return pixel * pow(2.0, (20.0 - u_Zoom));
   }
   return pixel;
 }
