df25712d084d6a29ba290d3e5ccb5c47976f72a3
thinkinggis <lzx199065@gmail.com>
2020-01-19 20:22:30 +0800

    fix: layer add,remove enent

--- packages/core/src/services/log/LogService.ts
+++ packages/core/src/services/log/LogService.ts
@@ -1,8 +1,7 @@
 import { injectable } from 'inversify';
 import Probe, { Log } from 'probe.gl';
 import { ILogService } from './ILogService';
-console.log(process.env.NODE_ENV)
-const Logger = new Log({ id: 'L7' }).enable(true);
+const Logger = new Log({ id: 'L7' }).enable(false);
 // // 只输出 debug 级别以上的日志信息
 Logger.priority = 5;
 
