4374a032278583a4bfe13f36360c94ebc4118915
thinkinggis <lzx199065@gmail.com>
2020-01-21 17:19:30 +0800

    fix: hightlight opacity

--- stories/Source/Map.stories.tsx
+++ stories/Source/Map.stories.tsx
@@ -1,10 +1,12 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
-import MultiPolygon from './components/multiPolygon';
 import MultiLine from './components/multiLine';
+import MultiPolygon from './components/multiPolygon';
+import UpdatePolygon from './components/updatedata';
 // @ts-ignore
 import notes from './Map.md';
 // @ts-ignore
 storiesOf('数据', module)
   .add('multiPolygon', () => <MultiPolygon />, {})
+  .add('updatePolygon', () => <UpdatePolygon />, {})
   .add('MultiLine', () => <MultiLine />, {});
