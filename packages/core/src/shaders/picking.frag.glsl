e5e520e8d0888a329f19bf510d586738380a6bf1
thinkinggis <lzx199065@gmail.com>
2020-02-05 10:35:22 +0800

    fix: point layer 地图高亮的问题

--- packages/core/src/shaders/picking.frag.glsl
+++ packages/core/src/shaders/picking.frag.glsl
@@ -16,11 +16,11 @@ vec4 filterHighlightColor(vec4 color) {
   if (selected) {
     vec4 highLightColor = u_HighlightColor * COLOR_SCALE;
 
-    // float highLightAlpha = highLightColor.a;
-    // float highLightRatio = highLightAlpha / (highLightAlpha + color.a * (1.0 - highLightAlpha));
+    float highLightAlpha = highLightColor.a;
+    float highLightRatio = highLightAlpha / (highLightAlpha + color.a * (1.0 - highLightAlpha));
 
-    // vec3 resultRGB = mix(color.rgb, highLightColor.rgb, highLightRatio);
-    return highLightColor;
+    vec3 resultRGB = mix(color.rgb, highLightColor.rgb, highLightRatio);
+    return vec4(resultRGB, color.a);
   } else {
     return color;
   }
