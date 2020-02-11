1b47eba8b316083fdce58539e98c3608263df07d
thinkinggis <lzx199065@gmail.com>
2020-02-11 16:35:18 +0800

    improvement: 点亮城市支持自定义动画频率&scene支持传入dom动画

--- packages/core/src/services/log/LogService.ts
+++ packages/core/src/services/log/LogService.ts
@@ -1,9 +1,9 @@
 import { injectable } from 'inversify';
 import Probe, { Log } from 'probe.gl';
 import { ILogService } from './ILogService';
-const Logger = new Log({ id: 'L7' }).enable(false);
+const Logger = new Log({ id: 'L7' }).enable(true);
 // // 只输出 debug 级别以上的日志信息
-Logger.priority = 5;
+Logger.priority = 2;
 
 @injectable()
 export default class LogService implements ILogService {
@@ -12,7 +12,7 @@ export default class LogService implements ILogService {
   }
 
   public warn(message: string): void {
-    Logger.warn(message)();
+    Logger.info(1, message)();
   }
 
   public info(message: string): void {
