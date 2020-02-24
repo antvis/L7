63153881fd6452f95cea7a2ce1a6341a7bc5685f
thinkinggis <lzx199065@gmail.com>
2020-02-24 14:54:29 +0800

    fix: l7 react source update 机制

--- packages/react/src/component/LayerAttribute/Filter.tsx
+++ packages/react/src/component/LayerAttribute/Filter.tsx
@@ -13,6 +13,6 @@ export default React.memo(function Chart(props: ILayerProps) {
     if (filter.field) {
       layer.filter(filter.field as string, filter.values as StyleAttrField);
     }
-  }, [filter.field, JSON.stringify(filter.values), filter.values]);
+  }, [filter.field, JSON.stringify(filter.values)]);
   return null;
 });
