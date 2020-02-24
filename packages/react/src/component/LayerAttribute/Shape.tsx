eeefe975c6727649ecae9ff5c4f86645c9bc628d
thinkinggis <lzx199065@gmail.com>
2020-02-24 14:54:29 +0800

    fix: l7 react source update 机制

--- packages/react/src/component/LayerAttribute/Shape.tsx
+++ packages/react/src/component/LayerAttribute/Shape.tsx
@@ -10,6 +10,7 @@ interface ILayerProps {
 export default React.memo(function Chart(props: ILayerProps) {
   const { layer, shape } = props;
   useEffect(() => {
+    console.log(shape);
     shape.field
       ? layer.shape(shape.field, shape.values)
       : layer.shape(shape.values as StyleAttrField);
