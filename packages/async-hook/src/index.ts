31c60da4b29775ca10ee8e215016b1039fb2dff0
thinkinggis <lzx199065@gmail.com>
2020-02-26 22:23:43 +0800

    chore: move out async-hook

--- packages/async-hook/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SyncHook } from './core/SyncHook';
-export { default as AsyncParallelHook } from './core/AsyncParallelHook';
-export { default as SyncWaterfallHook } from './core/SyncWaterfallHook';
-export { default as SyncBailHook } from './core/SyncBailHook';
