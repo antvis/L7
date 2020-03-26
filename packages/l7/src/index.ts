6be5b3aeffa9406421776e5787058935d0ee634f
thinkinggis <lzx199065@gmail.com>
2020-03-26 15:21:13 +0800

    chore: publish

--- packages/l7/src/index.ts
+++ packages/l7/src/index.ts
@@ -3,4 +3,4 @@ export * from '@antv/l7-scene';
 export * from '@antv/l7-maps';
 export * from '@antv/l7-layers';
 export * from '@antv/l7-component';
-export const version = '2.1.6';
+export const version = '2.1.7';
