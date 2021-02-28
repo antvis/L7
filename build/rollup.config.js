6a4035e960ab51f3a657e1de6a887415616eec73
thinkinggis <lzx199065@gmail.com>
2021-02-28 14:53:44 +0800

    chore(rollup): 更新rollup 配置

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -72,6 +72,7 @@ module.exports = [
       commonjs({
         namedExports: {
           eventemitter3: [ 'EventEmitter' ],
+          inversify: ['inject','injectable','postConstruct','Container','decorate','interfaces'],
           // @see https://github.com/rollup/rollup-plugin-commonjs/issues/266
           lodash: [
             'isNil',
@@ -84,7 +85,10 @@ module.exports = [
             'isNumber',
             'merge'
           ]
-        }
+        },
+        dynamicRequireTargets: [
+          'node_modules/inversify/lib/syntax/binding_{on,when}_syntax.js',
+        ],
       }),
       babel({
         extensions: [ '.js', '.ts' ]
--- packages/three/src/core/threeRenderService.ts
+++ packages/three/src/core/threeRenderService.ts
@@ -1,5 +1,6 @@
-import { IMapService, IMercator, IRendererService, TYPES } from '@antv/l7';
 import { inject, injectable } from 'inversify';
+import { IMapService, IMercator, IRendererService, TYPES } from '@antv/l7';
+
 import {
   AnimationMixer,
   Camera,
