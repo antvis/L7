f82ac514e2721a495f1aef451ff3ce178138726d
2912401452 <2912401452@qq.com>
2021-08-15 19:52:49 +0800

    style: demo code

--- stories/customMap/components/Map.tsx
+++ stories/customMap/components/Map.tsx
@@ -44,6 +44,7 @@ export default class ScaleComponent extends React.Component {
     );
     let imageLayer = new PointLayer({
       blend: 'normal',
+      zIndex: 2
     })
       .source(
         [
@@ -68,70 +69,71 @@ export default class ScaleComponent extends React.Component {
       .size(20);
 
     scene.on('loaded', () => {
+      fetch(
+        'https://gw.alipayobjects.com/os/basement_prod/d2e0e930-fd44-4fca-8872-c1037b0fee7b.json',
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          const textLayer = new PointLayer({})
+            .source(
+              [
+                {
+                  lng: 120.5,
+                  lat: 31.3,
+                  iconType: 'cloud',
+                  iconColor: '#F0F8FF',
+                  weather: '多云 - 今日适宜出门',
+                  textOffset: [-40, 0],
+                },
+              ],
+              {
+                parser: {
+                  type: 'json',
+                  x: 'lng',
+                  y: 'lat',
+                },
+              },
+            )
+            .shape('weather', 'text')
+            .size(16)
+            .color('#f00')
+            .style({
+              textAnchor: 'center', // 文本相对锚点的位置 center|left|right|top|bottom|top-left
+              textOffset: [0, 0], // 文本相对锚点的偏移量 [水平, 垂直]
+              spacing: 2, // 字符间距
+              padding: [1, 1], // 文本包围盒 padding [水平，垂直]，影响碰撞检测结果，避免相邻文本靠的太近
+              fontFamily: 'Times New Roman',
+              textAllowOverlap: true,
+            })
+            .active(true);
+          scene.addLayer(textLayer);
+  
+          const layer = new PolygonLayer({
+            name: '01',
+          });
+  
+          layer
+            .source(data)
+            .size('name', [0, 10000, 50000, 30000, 100000])
+            .color('name', [
+              '#2E8AE6',
+              '#69D1AB',
+              '#DAF291',
+              '#FFD591',
+              '#FF7A45',
+              '#CF1D49',
+            ])
+            .shape('fill')
+            .select(true)
+            .style({
+              opacity: 1.0,
+            });
+          scene.addLayer(layer);
+        });
       scene.addLayer(imageLayer);
     });
 
-    fetch(
-      'https://gw.alipayobjects.com/os/basement_prod/d2e0e930-fd44-4fca-8872-c1037b0fee7b.json',
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const textLayer = new PointLayer({})
-          .source(
-            [
-              {
-                lng: 120.5,
-                lat: 31.3,
-                iconType: 'cloud',
-                iconColor: '#F0F8FF',
-                weather: '多云 - 今日适宜出门',
-                textOffset: [-40, 0],
-              },
-            ],
-            {
-              parser: {
-                type: 'json',
-                x: 'lng',
-                y: 'lat',
-              },
-            },
-          )
-          .shape('weather', 'text')
-          .size(16)
-          .color('#f00')
-          .style({
-            textAnchor: 'center', // 文本相对锚点的位置 center|left|right|top|bottom|top-left
-            textOffset: [0, 0], // 文本相对锚点的偏移量 [水平, 垂直]
-            spacing: 2, // 字符间距
-            padding: [1, 1], // 文本包围盒 padding [水平，垂直]，影响碰撞检测结果，避免相邻文本靠的太近
-            fontFamily: 'Times New Roman',
-            textAllowOverlap: true,
-          })
-          .active(true);
-        scene.addLayer(textLayer);
-
-        const layer = new PolygonLayer({
-          name: '01',
-        });
-
-        layer
-          .source(data)
-          .size('name', [0, 10000, 50000, 30000, 100000])
-          .color('name', [
-            '#2E8AE6',
-            '#69D1AB',
-            '#DAF291',
-            '#FFD591',
-            '#FF7A45',
-            '#CF1D49',
-          ])
-          .shape('fill')
-          .select(true)
-          .style({
-            opacity: 1.0,
-          });
-        scene.addLayer(layer);
-      });
+   
   }
 
   public render() {
