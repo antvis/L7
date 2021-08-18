51b2ee578a5ec31013b3d3c8b96b67fdf12ef3ce
zhongwu <xingya.sxy@alibaba-inc.com>
2021-08-18 23:32:56 +0800

    feat: 修改版本引用

--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -1,8 +1,8 @@
 // import GaodeMap from './amap/';
-import GaodeMap from './amap/';
+// import GaodeMap from './amap/';
 // import GaodeMapV1 from './amap/';
-import GaodeMapV2 from './amap2/';
+// import GaodeMapV2 from './amap2/';
 import Map from './map/';
-import Mapbox from './mapbox/';
+// import Mapbox from './mapbox/';
 
-export { GaodeMap, GaodeMapV2, Mapbox, Map };
+export { Map };
