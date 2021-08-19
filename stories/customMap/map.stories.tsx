af67a37aefa0dae8733bcd1ca7bbd531c3c05275
2912401452 <2912401452@qq.com>
2021-08-19 18:19:22 +0800

    feat: demo

--- stories/customMap/map.stories.tsx
+++ stories/customMap/map.stories.tsx
@@ -2,5 +2,7 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import Map from './components/Map';
+import Map2 from './components/Map2';
 // @ts-ignore
-storiesOf('自定义地图', module).add('地图', () => <Map />);
+storiesOf('自定义地图', module).add('地图', () => <Map />)
+.add('地图2', () => <Map2 />);
