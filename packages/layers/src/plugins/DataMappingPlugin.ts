29a11b2196dc4b9b1171fc41b3897a2935323f5b
2912401452 <2912401452@qq.com>
2021-09-14 11:16:59 +0800

    style: lint style

--- packages/layers/src/plugins/DataMappingPlugin.ts
+++ packages/layers/src/plugins/DataMappingPlugin.ts
@@ -151,29 +151,29 @@ export default class DataMappingPlugin implements ILayerPlugin {
         // 单个的点数据
         // @ts-ignore
         mappedData
-        // TODO: 避免经纬度被重复计算导致坐标位置偏移
-        .filter((d) => !d.originCoordinates)
-        .map((d) => {
-          d.version = 'GAODE2.x';
-          // @ts-ignore
-          d.originCoordinates = cloneDeep(d.coordinates); // 为了兼容高德1.x 需要保存一份原始的经纬度坐标数据（许多上层逻辑依赖经纬度数据）
-          // @ts-ignore
-          d.coordinates = this.mapService.lngLatToCoord(d.coordinates);
-          // d.coordinates = this.mapService.lngLatToCoord(unProjectFlat(d.coordinates));
-        });
+          // TODO: 避免经纬度被重复计算导致坐标位置偏移
+          .filter((d) => !d.originCoordinates)
+          .map((d) => {
+            d.version = 'GAODE2.x';
+            // @ts-ignore
+            d.originCoordinates = cloneDeep(d.coordinates); // 为了兼容高德1.x 需要保存一份原始的经纬度坐标数据（许多上层逻辑依赖经纬度数据）
+            // @ts-ignore
+            d.coordinates = this.mapService.lngLatToCoord(d.coordinates);
+            // d.coordinates = this.mapService.lngLatToCoord(unProjectFlat(d.coordinates));
+          });
       } else {
         // 连续的线、面数据
         // @ts-ignore
         mappedData
-        // TODO: 避免经纬度被重复计算导致坐标位置偏移
-        .filter((d) => !d.originCoordinates)
-        .map((d) => {
-          d.version = 'GAODE2.x';
-          // @ts-ignore
-          d.originCoordinates = cloneDeep(d.coordinates); // 为了兼容高德1.x 需要保存一份原始的经纬度坐标数据（许多上层逻辑依赖经纬度数据）
-          // @ts-ignore
-          d.coordinates = this.mapService.lngLatToCoords(d.coordinates);
-        });
+          // TODO: 避免经纬度被重复计算导致坐标位置偏移
+          .filter((d) => !d.originCoordinates)
+          .map((d) => {
+            d.version = 'GAODE2.x';
+            // @ts-ignore
+            d.originCoordinates = cloneDeep(d.coordinates); // 为了兼容高德1.x 需要保存一份原始的经纬度坐标数据（许多上层逻辑依赖经纬度数据）
+            // @ts-ignore
+            d.coordinates = this.mapService.lngLatToCoords(d.coordinates);
+          });
       }
     }
     // console.log('mappedData', mappedData)
