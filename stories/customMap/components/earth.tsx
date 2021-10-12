7bfdb4b7c1e0a4720a461234e405fc909c390863
YiQianYao <42212176+2912401452@users.noreply.github.com>
2021-10-12 14:39:08 +0800

    Shihui dev (#797)
    * feat: add getModelMatrix into viewport
    
    * feat: 新增地球模式 （初步构建）
    
    * feat: 完善地球交互
    
    * style: lint style
    
    * feat: 调整地球图层缩放的方向
    
    * style: lint style
    
    * feat: 增加地球模式的 pointLayer/fill 图层
    
    * style: lint style
    
    * feat: 增加地球、太阳的简单运动系统，优化部分代码结构
    
    * fix: 修复时间点击出错
    
    * style: lint style
    
    * fix: 修复地图 panBy 方法参数错误
    
    * style: lint style
    
    * feat: pointLayer/cylinder 圆柱兼容地球模式
    
    * style: lint style
    
    * feat: 修复 pointLayer/fill 在拾取是破面严重的情况
    
    * style: lint style
    
    * feat: 增加 arc 弧度调节
    
    * feat: 增加 lineLayer/arc3d 兼容地球模式
    
    * style: lint style
    
    * feat: 增加地球图层 - 大气层
    
    * style: lint style
    
    * feat: 增加设置可视化层背景色的能力
    
    * style: lint style
    
    * feat: 增加地球外发光效果
    
    * style: lint style
    
    * feat: 允许用户不使用 layer 的 source 方法 - 地球模式下光晕图层不需要传数据
    
    * style: lint style
    
    * feat: 调整光晕的 shader 计算
    
    * chore: update version 2.5.32 -> 2.5.33
    
    * feat: 调整地球大气层的渲染层级
    
    * style: lint style
    
    * feat: 调整案例
    
    * style: lint style

--- stories/customMap/components/earth.tsx
+++ stories/customMap/components/earth.tsx
@@ -22,6 +22,28 @@ export default class ScaleComponent extends React.Component {
     });
 
     var d = [
+      { lng: 121.61865234375, lat: 25.29437116258816 },
+      { lng: 121.058349609375, lat: 25.015928763367857 },
+      { lng: 120.7177734375, lat: 24.587090339209634 },
+      { lng: 120.28930664062499, lat: 23.936054914599815 },
+      { lng: 120.12451171875, lat: 23.553916518321625 },
+      { lng: 120.08056640625, lat: 23.120153621695614 },
+      { lng: 120.234375, lat: 22.867317960075614 },
+      { lng: 120.43212890625, lat: 22.52270570348246 },
+      { lng: 120.65185546875, lat: 22.370396344320053 },
+      { lng: 120.750732421875, lat: 21.922663209325922 },
+      { lng: 120.948486328125, lat: 22.268764039073968 },
+      { lng: 121.124267578125, lat: 22.806567100271522 },
+      { lng: 121.56372070312499, lat: 23.915970370510227 },
+      { lng: 121.88232421875, lat: 24.557116164309626 },
+      { lng: 121.95922851562501, lat: 25.075648445630527 },
+      { lng: 109.97314453125, lat: 20.076570104545173 },
+      { lng: 108.896484375, lat: 19.663280219987662 },
+      { lng: 108.61083984375, lat: 18.979025953255267 },
+      { lng: 108.80859375, lat: 18.47960905583197 },
+      { lng: 109.599609375, lat: 18.35452552912664 },
+      { lng: 110.32470703125, lat: 18.771115062337024 },
+      { lng: 111.005859375, lat: 19.78738018198621 },
       { lng: 110, lat: 30 },
       { lng: 127.657407, lat: 49.76027 },
       { lng: 129.397818, lat: 49.4406 },
@@ -352,11 +374,11 @@ export default class ScaleComponent extends React.Component {
           },
         },
       )
-      .shape('circle')
-      // .shape('cylinder')
+      // .shape('circle')
+      .shape('cylinder')
       .color('#f00')
-      // .size('', () => [1, 1, 10])
-      .size(20)
+      .size('', () => [1, 1, 10])
+      // .size(20)
       .style({
         // opacity: 0.6,
       })
@@ -407,13 +429,25 @@ export default class ScaleComponent extends React.Component {
           // earthTime: 4.0
           earthTime: 0.1,
         },
-      });
-    // .animate(true);
+      })
+      .animate(true);
     // earthlayer.setEarthTime(4.0)
+
+    const atomLayer = new EarthLayer()
+      .color('#2E8AE6')
+      .shape('atomSphere')
+      .style({
+        opacity: 1,
+      });
+
+    const bloomLayer = new EarthLayer().color('#fff').shape('bloomSphere');
+
     scene.on('loaded', () => {
       scene.addLayer(earthlayer);
       scene.addLayer(pointlayer);
-      // console.log(pointlayer)
+
+      scene.addLayer(atomLayer);
+      scene.addLayer(bloomLayer);
 
       earthlayer.setEarthTime(4.0);
     });
