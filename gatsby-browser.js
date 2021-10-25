64bfbb459212335aab72916c62fb5af0f1df4309
shihui <yiqianyao.yqy@alibaba-inc.com>
2021-10-25 18:09:08 +0800

    feat: 增加点击建筑 demo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,6 +12,7 @@ window.l7District = require('@antv/l7-district');
 window.l7Three = require('@antv/l7-three');
 window.three = require('three');
 window.GLTFLoader = require('three/examples/jsm/loaders/GLTFLoader');
+window.FBXLoader = require('three/examples/jsm/loaders/FBXLoader');
 window.react = require('react');
 window.popmotion = require('popmotion');
 window.reactDom = require('react-dom');
