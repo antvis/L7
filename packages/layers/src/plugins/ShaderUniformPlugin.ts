0ecb2b114ed78b1b0b3b1c888828a72b910495f2
2912401452 <2912401452@qq.com>
2021-10-28 19:18:34 +0800

    style: lint style

--- packages/layers/src/plugins/ShaderUniformPlugin.ts
+++ packages/layers/src/plugins/ShaderUniformPlugin.ts
@@ -79,7 +79,7 @@ export default class ShaderUniformPlugin implements ILayerPlugin {
           // u_ModelMatrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
           u_PickingBuffer: layer.getLayerConfig().pickingBuffer || 0,
           // TODO: 当前地图是否在拖动
-          u_Dragging: Number(this.mapService.dragging)
+          u_Dragging: Number(this.mapService.dragging),
         });
       });
 
