8c62feb2c233f36dc2639a0a7f1c9ac605a133cc
2912401452 <2912401452@qq.com>
2021-10-29 17:15:51 +0800

    feat: remove test hammerjs

--- packages/core/src/services/interaction/InteractionService.ts
+++ packages/core/src/services/interaction/InteractionService.ts
@@ -2,7 +2,6 @@ import { $window, isMini } from '@antv/l7-utils';
 import EventEmitter from 'eventemitter3';
 import { inject, injectable } from 'inversify';
 import Hammer from 'l7hammerjs'; // l7 - mini
-// import Hammer from 'hammerjs'; // l7 - mini
 import 'reflect-metadata';
 // @ts-ignore
 import { TYPES } from '../../types';
