4d9734c69f6ecda7cc62459a28dd490ac3194ee1
2912401452 <2912401452@qq.com>
2021-11-08 15:22:57 +0800

    feat: 完善站点构建 增加 @antv/l7-maps/lib/map 的引用

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,7 @@ window.geotiff = require('geotiff');
 window.g2 = require('@antv/g2');
 window.l7 = require('@antv/l7');
 window.l7Mini = require('@antv/l7-mini');
+window.l7MiniMap = require('@antv/l7-maps/lib/map');
 window.l7Maps = require('@antv/l7-maps');
 window.l7React = require('@antv/l7-react');
 window.l7Draw = require('@antv/l7-draw');
