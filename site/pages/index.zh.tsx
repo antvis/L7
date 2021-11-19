8880f58edde08e88a6c244e5e981d1fb49c85b0c
wb-yzg904416@antgroup.com <wb-yzg904416@antgroup.com>
2021-11-19 20:10:43 +0800

    添加两个组件

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -6,6 +6,8 @@ import Features from '@antv/gatsby-theme-antv/site/components/Features';
 import SEO from '@antv/gatsby-theme-antv/site/components/Seo';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Dipper } from '../components/Dipper';
+import { DipperMap } from '../components/DipperMap';
 import '../css/home.css';
 
 const IndexPage = () => {
@@ -127,6 +129,42 @@ const IndexPage = () => {
     },
   ];
 
+  const dipper = [
+    {
+      title: t('指挥监控场景'),
+      image: 'https://gw.alipayobjects.com/zos/bmw-prod/a8d32053-ef9d-485e-ae13-2b49535e6f4f.svg',
+      link: ''
+    },
+    {
+      title: t('指挥分配场景'),
+      image: 'https://gw.alipayobjects.com/zos/bmw-prod/4235cc53-ef5f-4a47-a33c-1623df19e4b7.svg',
+      link: ''
+    },
+    {
+      title: t('可视化配置场景'),
+      image: 'https://gw.alipayobjects.com/zos/bmw-prod/29cbd68c-86ac-440c-a38d-792dbd8aea61.svg',
+      link: ''
+    }
+  ];
+
+  const dippermap = [
+    {
+      title: '点',
+      image: 'https://gw.alipayobjects.com/mdn/rms_58ab56/afts/img/A*PYEFRqqXZ98AAAAAAAAAAAAAARQnAQ',
+      link: ''
+    },
+    {
+      title: '线',
+      image: 'https://gw.alipayobjects.com/mdn/rms_58ab56/afts/img/A*5CdCTqtw4dgAAAAAAAAAAAAAARQnAQ',
+      link: ''
+    },
+    {
+      title: '面',
+      image: 'https://gw.alipayobjects.com/mdn/rms_58ab56/afts/img/A*o7fMQbQiNLEAAAAAAAAAAAAAARQnAQ',
+      link: ''
+    }
+  ];
+
   return (
     <>
       <SEO title={t('蚂蚁地理空间数据可视化')} lang={i18n.language} />
@@ -148,7 +186,9 @@ const IndexPage = () => {
         githubStarLink="https://github.com/antvis/L7/stargazers"
       />
       <Features features={features} style={{ width: '100%' }} />
+      <Dipper dipper={dipper} />
       <Cases cases={cases} />
+      <DipperMap dippermap={dippermap} />
       <Companies title={t('感谢信赖')} companies={companies} />
     </>
   );
