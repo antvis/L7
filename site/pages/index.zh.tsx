64d3ef2323ffdf344542d0f893b9e856007f84c8
@thinkinggis <lzx199065@gmail.com>
2021-11-23 13:38:48 +0800

    Fix site map (#847)
    * fix: 解决方案页面懒加载
    
    * fix: 添加demo
    
    * 添加两个组件
    
    * fix: 修改样式
    
    * fix: 样式调整
    
    * fix: 更新官网
    
    * fix: 修改样式
    
    * fix: 更新首页
    
    * docs: 优化demo
    
    Co-authored-by: wb-yzg904416@antgroup.com <wb-yzg904416@antgroup.com>

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -6,14 +6,89 @@ import Features from '@antv/gatsby-theme-antv/site/components/Features';
 import SEO from '@antv/gatsby-theme-antv/site/components/Seo';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useConfig } from './useConfig';
 import { Dipper } from '../components/Dipper';
 import { DipperMap } from '../components/DipperMap';
 import '../css/home.css';
 
+
 const IndexPage = () => {
   const { t, i18n } = useTranslation();
-  const { L7Case, L7Features,notifications,companies, ecosystems} = useConfig();
+  const L7Case = [
+    {
+      logo:
+        'https://antv-2018.alipay.com/assets/image/icon/l7.svg',
+      title: t('指挥分配场景'),
+      description: t(
+        '区域化网格化数据管理指挥分配场景',
+      ),
+      link: `https://antv.vision/Dipper/~demos/docs-task`,
+      image:
+        'https://gw.alipayobjects.com/mdn/rms_08cc33/afts/img/A*scJBTq8PW7kAAAAAAAAAAAAAARQnAQ',
+    },
+    {
+      logo:
+        'https://antv-2018.alipay.com/assets/image/icon/l7.svg',
+      title: t('地图数据分析'),
+      description: t(
+        '区域化网格化数据分析场景',
+      ),
+      link: `https://antv.vision/Dipper/~demos/docs-analysis`,
+      image:
+        'https://gw.alipayobjects.com/mdn/rms_08cc33/afts/img/A*OnGVRb_qWxcAAAAAAAAAAAAAARQnAQ',
+    },
+    {
+      logo:
+        'https://gw.alipayobjects.com/zos/bmw-prod/222865fc-15e9-44b9-b726-444e1512d937.ico',
+      title: t('DipperMap 地理分析工具'),
+      description: t(
+        '地图可视化配置分析类场景',
+      ),
+      link: `/${i18n.language}/examples/gallery/basic`,
+      image:
+        'https://gw.alipayobjects.com/mdn/rms_e7e1c6/afts/img/A*MPWKQqh54vwAAAAAAAAAAAAAARQnAQ',
+    },
+  ];
+  const companies = [{
+    name: '阿里云',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*V_xMRIvw2iwAAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: '支付宝',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*lYDrRZvcvD4AAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: '天猫',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*BQrxRK6oemMAAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: '淘宝网',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*1l8-TqUr7UcAAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: '网商银行',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*ZAKFQJ5Bz4MAAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: '盒马',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*ePJMQZCb8vkAAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: 'yunos',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*_js7SaNosUwAAAAAAAAAAABkARQnAQ',
+  },
+  {
+    name: '菜鸟',
+    img:
+      'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*TgV-RZDODJIAAAAAAAAAAABkARQnAQ',
+  }
+  ];
 
   const bannerButtons = [
     {
@@ -26,7 +101,36 @@ const IndexPage = () => {
       link: `/${i18n.language}/docs/api/l7`,
     },
   ];
-
+  const L7Features = [
+    {
+      icon:
+        'https://gw.alipayobjects.com/zos/basement_prod/ca2168d1-ae50-4929-8738-c6df62231de3.svg',
+      title: t('架构灵活且自由'),
+      description: t('支持地图底图，渲染引擎，图层自由定制、扩展，组合'),
+    },
+    {
+      text: t('图表示例'),
+      link: `/${i18n.language}/examples/gallery`,
+      type: 'primary',
+    },
+    {
+      text: t('开始使用'),
+      link: `/${i18n.language}/docs/api/l7`,
+    },
+  ];
+  const notifications = [{
+    type: t('DipperMap'),
+    title: t('地理可视化工具'),
+    date: '2021.11.22',
+    link: 'https://dippermap.alipay.com/',
+  },
+  {
+    type: t('L7 一站式地理分析应用解决方案'),
+    title: t('L7Mini、Dipper、DipperMap'),
+    date: '2021.11.22',
+    link: 'https://www.yuque.com/antv/blog/xqslop',
+  },
+  ]
 
   const dipper = [
     {
@@ -104,7 +208,6 @@ const IndexPage = () => {
       <Cases cases={L7Case} />
       <DipperMap dippermap={dippermap} />
       <Companies title={t('感谢信赖')} companies={companies} />
-   
     </>
   );
 };
