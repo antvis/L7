1fcd1f06a074282a4a21c52beafa3b07beec49aa
YiQianYao <42212176+2912401452@users.noreply.github.com>
2021-12-21 15:48:35 +0800

    Shihuidev (#888)
    * feat: 增加着色器的拾取计算控制、完善 arcmini
    
    * feat: 完善 enableShaderPick/disableShaderPick 功能
    
    * style: lint style
    
    * feat: 补充调用高德地图公交线路查询 demo
    
    * style: lint style
    
    * feat: 优化弧线的纹理动画
    
    * style: lint style
    
    * feat: 去除greatCircle 的纹理动画优化
    
    * feat: 扩展点图层圆柱效果
    
    * feat: 增加几何体的径向渐变配置
    
    * style: lint style
    
    * fix: 修复bug 图层触发的事件跟图层设置的zIndex无关，只跟插入图层先后顺序有关
    
    * style: lint style
    
    * feat: 补全挤出几何体拾取颜色的光照配置
    
    * style: lint style
    
    * fix: 修复圆柱 cull 问题 mapbox amap 不同
    
    * feat: 图层销毁时的内存泄漏
    
    * style: lint style
    
    * feat: 平面弧线新增弧线偏移量的数据映射能力
    
    * style: lint style
    
    * fix: 修复重复销毁bug
    
    * style: lint style
    
    * feat: 修复 texture 重复销毁问题
    
    * style: lint style
    
    * fix: 修复图层叠加模式下的拾取失效问题
    
    * style: lint style
    
    * fix: 修复纹理贴图在 zoom 大于 12 时存在的问题
    
    * fix: 修复水波点颜色偏暗
    
    * feat: 优化点图层的渲染效果，叠加渲染效果
    
    * style: lint style
    
    * fix: 修复 layer contextmenu 事件丢失
    
    * fix: 修复 map 类型 resize 失效
    
    * style: lint style
    
    * feat: 增加瓦片地图的请求节流
    
    * style: lint style
    
    * feat: 优化热力图在 radius 数值比较大时热力点边缘发生裁剪的现象
    
    * style: lint style
    
    * fix: 修复resize 后 picking shiqu 拾取失败的问题
    
    * feat: 优化 marker/popup 在容器边缘的表现
    
    * feat: 增加 setEnableRender 方法
    
    * style: lint style
    
    * feat: 增加城市图层扫光特效
    
    * style: lint style
    
    * feat: 补全拾取色混合配置
    
    * style: lint style
    
    * feat: 增加高德地图的面积大小点
    
    * style: lint style
    
    * feat: 点优化边缘锯齿
    
    * feat: add declaration layers
    
    * feat: 调整点图层 version 的引入
    
    * style: lint style
    
    * fix: 修复pointLayer stroke 变暗问题

--- packages/layers/src/point/models/fill.ts
+++ packages/layers/src/point/models/fill.ts
@@ -23,8 +23,8 @@ import pointFillVert from '../shaders/fill_vert.glsl';
 
 import { isNumber } from 'lodash';
 
+import { Version } from '@antv/l7-maps';
 import { mat4, vec3 } from 'gl-matrix';
-import { Version } from '../../../../maps/src/version';
 interface IPointLayerStyleOptions {
   opacity: styleSingle;
   strokeWidth: styleSingle;
