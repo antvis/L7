b2bf06ee869fb2e59bead42eb780511ffbb473a8
lviser <yunji.me@outlook.com>
2022-02-11 11:28:33 +0800

    chore(test demo): lineLayer date update (#958)

--- stories/MapPerformance/map.stories.tsx
+++ stories/MapPerformance/map.stories.tsx
@@ -2,8 +2,10 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import PointTest from './components/Map';
-import BigLine from './components/BigLine'
+import BigLine from './components/BigLine';
+import DataUpdate from './components/DataUpdate';
 // @ts-ignore
 storiesOf('地图性能检测', module)
-.add('点', () => <PointTest />)
-.add('BigLine', () => <BigLine />)
+  .add('点', () => <PointTest />)
+  .add('BigLine', () => <BigLine />)
+  .add('DataUpdate', () => <DataUpdate />);
