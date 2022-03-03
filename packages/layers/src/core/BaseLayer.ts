d957ae7ca1cc22a8cebe1cd5423eae9844527d0f
YiQianYao <42212176+2912401452@users.noreply.github.com>
2022-03-03 11:52:41 +0800

    Shihui (#989)
    * chore: update version 2.7.30 -> 2.7.31
    
    * fix: 修复 高德 1.x 地图时存在的可视化层延迟的现象
    
    * style: lint style

--- packages/layers/src/core/BaseLayer.ts
+++ packages/layers/src/core/BaseLayer.ts
@@ -299,6 +299,14 @@ export default class BaseLayer<ChildLayerStyleOptions = {}> extends EventEmitter
       TYPES.IPickingService,
     );
     this.mapService = this.container.get<IMapService>(TYPES.IMapService);
+    const { enableMultiPassRenderer, passes } = this.getLayerConfig();
+    if (enableMultiPassRenderer && passes?.length && passes.length > 0) {
+      // Tip: 兼容 multiPassRender 在 amap1 时存在的图层不同步问题 zoom
+      this.mapService.on('mapAfterFrameChange', () => {
+        this.renderLayers();
+      });
+    }
+
     this.cameraService = this.container.get<ICameraService>(
       TYPES.ICameraService,
     );
