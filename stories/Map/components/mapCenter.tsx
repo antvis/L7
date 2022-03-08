4553a5cae16a24f298c718c10fe1c73fc42f099f
2912401452 <2912401452@qq.com>
2022-03-08 10:10:42 +0800

    style: lint style

--- stories/Map/components/mapCenter.tsx
+++ stories/Map/components/mapCenter.tsx
@@ -56,94 +56,60 @@ export default class GaodeMapComponent extends React.Component {
     //   .size(2)
     //   .color('#000');
 
-    // let text = new PointLayer({ zIndex: 2 })
-    //   .source(
-    //     [
-    //       {
-    //         lng: aspaceLnglat[0] + 0.0002,
-    //         lat: aspaceLnglat[1],
-    //       },
-    //     ],
-    //     {
-    //       parser: {
-    //         type: 'json',
-    //         x: 'lng',
-    //         y: 'lat',
-    //       },
-    //     },
-    //   )
-    //   // .shape('100m', 'text')
-    //    .shape('circle')
-    //   .size(25)
-    //   .color('#0f0')
-    //   .style({
-    //     textOffset: [50, 20],
-    //   });
-
-    // let layer = new PointLayer({}) // blend: 'additive'
-    //   .source(
-    //     [
-    //       // {
-    //       //   lng: 120,
-    //       //   lat: 30.267069,
-    //       //   name: 'n1'
-    //       // },
-    //       // {
-    //       //   lng: 120.1025,
-    //       //   lat: 30.264701434772807,
-    //       //   name: 'n2',
-    //       // },
-    //       {
-    //         lng: 120.1019811630249,
-    //         lat: 30.264701434772807,
-    //         name: 'n3',
-    //       },
-    //     ],
-    //     {
-    //       parser: {
-    //         type: 'json',
-    //         x: 'lng',
-    //         y: 'lat',
-    //       },
-    //     },
-    //   )
-    //   .shape('circle')
-    //   // .shape('cylinder')
-    //   // .color('#0f9')
-    //   // .color('#4169E1')
-    //   // .color('#4cfd47')
-    //   .color('name', ['#f00', '#ff0'])
-    //   // .size([100, 100, 1000])
-    //   // .size([100, 100, 1000])
-    //   // .size(10)
-    //   .size('name', [20, 40])
-    //   // .animate({
-    //   //   // enable: true,
-    //   //   enable: false,
-    //   //   // type: 'www'
-    //   // })
-    //   // .animate(true)
-    //   .select(true)
-    //   .active({ color: '#00f' })
-    //   .style({
-    //     heightfixed: true,
-    //     // pickLight: false,
-    //     pickLight: true,
-    //     // lightEnable: true,
-    //     // opacity: 0.5,
-    //     stroke: '#f00',
-    //     // strokeWidth: 10,
-    //     strokeWidth: 0,
-    //     strokeOpacity: 1,
-    //     // unit: 'meter',
-    //   });
-    // // .animate(true)
-    // // .animate({
-    // //   enable: true,
-    // //   speed: 0.02,
-    // //   repeat: 1
-    // // })
-    // // .active({ color: '#00f' });
+    let layer = new PointLayer({}) // blend: 'additive'
+      .source(
+        [
+          {
+            lng: 120.1019811630249,
+            lat: 30.264701434772807,
+            name: 'n3',
+          },
+        ],
+        {
+          parser: {
+            type: 'json',
+            x: 'lng',
+            y: 'lat',
+          },
+        },
+      )
+      // .shape('circle')
+      .shape('cylinder')
+      // .color('#0f9')
+      // .color('#4169E1')
+      .color('#4cfd47')
+      // .color('name', ['#f00', '#ff0'])
+      // .size([100, 100, 1000])
+      .size([20, 20, 200])
+      // .size(10)
+      // .size('name', [20, 40])
+      // .animate({
+      //   // enable: true,
+      //   enable: false,
+      //   // type: 'www'
+      // })
+      // .animate(true)
+      .select(true)
+      .active({ color: '#ff0' })
+      .style({
+        // heightfixed: true,
+        // pickLight: false,
+        // pickLight: true,
+        // lightEnable: true,
+        // opacity: 0.5,
+        stroke: '#f00',
+        // strokeWidth: 10,
+        strokeWidth: 0,
+        strokeOpacity: 1,
+        // unit: 'meter',
+      });
+    // .animate(true)
+    // .animate({
+    //   enable: true,
+    //   speed: 0.02,
+    //   repeat: 1
+    // })
+    // .active({ color: '#00f' });
 
     this.scene = scene;
 
@@ -176,57 +142,11 @@ export default class GaodeMapComponent extends React.Component {
     // );
 
     scene.on('loaded', () => {
-      // scene.addLayer(layer);
+      scene.addLayer(layer);
 
       // let scale = layer.getScale('size');
       // console.log('scale n2', scale('n2'));
       // console.log('scale n3', scale('n3'));
-
-      let layer = new PointLayer({}) // blend: 'additive'
-        .source(
-          [
-            {
-              lng: 120.1019811630249,
-              lat: 30.264701434772807,
-              name: 'n3',
-            },
-          ],
-          {
-            parser: {
-              type: 'json',
-              x: 'lng',
-              y: 'lat',
-            },
-          },
-        )
-        .shape('circle')
-        .color('#ff0')
-        .animate(true)
-        .size(20);
-      scene.addLayer(layer);
-
-      let layer2 = new PointLayer({}) // blend: 'additive'
-        .source(
-          [
-            {
-              lng: 120.1019811630249 + 0.0001,
-              lat: 30.264701434772807,
-              name: 'n3',
-            },
-          ],
-          {
-            parser: {
-              type: 'json',
-              x: 'lng',
-              y: 'lat',
-            },
-          },
-        )
-        .shape('circle')
-        .color('#ff0')
-        .animate(true)
-        .size(20);
-      scene.addLayer(layer2);
 
       // let text = new PointLayer({ zIndex: 2 })
       //   .source(
