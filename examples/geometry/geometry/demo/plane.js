e666c043e78265a7e9e5f01824718e5f45d79443
2912401452 <2912401452@qq.com>
2022-03-25 22:14:40 +0800

    style: lint style

--- examples/geometry/geometry/demo/plane.js
+++ examples/geometry/geometry/demo/plane.js
@@ -4,7 +4,7 @@ import { GaodeMap } from '@antv/l7-maps';
 const scene = new Scene({
   id: 'map',
   map: new GaodeMap({
-    center: [120.1025, 30.2594],
+    center: [ 120.1025, 30.2594 ],
     style: 'dark',
     zoom: 10
   })
@@ -12,14 +12,14 @@ const scene = new Scene({
 
 scene.on('loaded', () => {
   const layer = new GeometryLayer()
-  .shape('plane')
-  .style({
-    opacity: 0.8,
-    width: 0.074,
-    height: 0.061,
-    center: [120.1025, 30.2594],
-  })
-  .active(true)
-  .color('#ff0');
+    .shape('plane')
+    .style({
+      opacity: 0.8,
+      width: 0.074,
+      height: 0.061,
+      center: [ 120.1025, 30.2594 ]
+    })
+    .active(true)
+    .color('#ff0');
   scene.addLayer(layer);
 });
--- examples/geometry/geometry/demo/terrain.js
+++ examples/geometry/geometry/demo/terrain.js
@@ -4,36 +4,36 @@ import { GaodeMap } from '@antv/l7-maps';
 const scene = new Scene({
   id: 'map',
   map: new GaodeMap({
-    center: [120.1025, 30.2594],
+    center: [ 120.1025, 30.2594 ],
     style: 'dark',
     pitch: 65,
     rotation: 180,
-    zoom: 14,
+    zoom: 14
   })
 });
 
 scene.on('loaded', () => {
   const layer = new GeometryLayer()
-  .shape('plane')
-  .style({
-    width: 0.074,
-    height: 0.061,
-    center: [120.1025, 30.2594],
-    widthSegments: 200,
-    heightSegments: 200,
-    terrainClipHeight: 1,
-    mapTexture:
+    .shape('plane')
+    .style({
+      width: 0.074,
+      height: 0.061,
+      center: [ 120.1025, 30.2594 ],
+      widthSegments: 200,
+      heightSegments: 200,
+      terrainClipHeight: 1,
+      mapTexture:
       'https://gw.alipayobjects.com/mdn/rms_23a451/afts/img/A*gA0NRbuOF5cAAAAAAAAAAAAAARQnAQ',
-    terrainTexture:
+      terrainTexture:
       'https://gw.alipayobjects.com/mdn/rms_23a451/afts/img/A*eYFaRYlnnOUAAAAAAAAAAAAAARQnAQ',
-    rgb2height: (r, g, b) => {
-      let h =
+      rgb2height: (r, g, b) => {
+        let h =
         -10000.0 +
         (r * 255.0 * 256.0 * 256.0 + g * 255.0 * 256.0 + b * 255.0) * 0.1;
-      h = h / 20 - 127600;
-      h = Math.max(0, h);
-      return h;
-    },
-  });
+        h = h / 20 - 127600;
+        h = Math.max(0, h);
+        return h;
+      }
+    });
   scene.addLayer(layer);
 });
