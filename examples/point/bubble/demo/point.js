1f833c268a61129d5cac36ad1d19f0fc1626aa2b
YiQianYao <42212176+2912401452@users.noreply.github.com>
2022-03-29 18:44:34 +0800

    Shihui (#1025)
    * style: lint style
    
    * style: lint style
    
    * docs: 增加官网关于 setCenter 的 demo/文档
    
    * style: lint style
    
    * docs: 增加 3D 填充图关于抬升配置和纹理配置的 demo 和 文档

--- examples/point/bubble/demo/point.js
+++ examples/point/bubble/demo/point.js
@@ -13,6 +13,7 @@ const scene = new Scene({
   })
 });
 scene.on('loaded', () => {
+  const plane = initPlane();
   fetch(
     'https://gw.alipayobjects.com/os/basement_prod/d3564b06-670f-46ea-8edb-842f7010a7c6.json'
   )
@@ -31,5 +32,45 @@ scene.on('loaded', () => {
           strokeWidth: 1
         });
       scene.addLayer(pointLayer);
+      pointLayer.on('click', e => {
+        const { lng, lat } = e.lngLat;
+        scene.setCenter([ lng, lat ], {
+          padding: {
+            right: 120
+          }
+        });
+        plane.style.right = '0px';
+        plane.innerHTML = `
+          <p>click info</>
+          <p>featureId: ${e.featureId}</p>
+          <p>lng: ${lng}</p>
+          <p>lat: ${lat}</p>
+        `;
+      });
+      pointLayer.on('unclick', () => {
+        plane.style.right = '-120px';
+        scene.setCenter([ 140.067171, 36.26186 ], {
+          padding: {
+            right: 0
+          }
+        });
+      });
     });
 });
+
+function initPlane() {
+  const el = document.createElement('div');
+  el.style.background = '#fff';
+  el.style.position = 'absolute';
+  el.style.padding = '10px';
+  el.style.top = '0';
+  el.style.right = '-120px';
+  el.style.width = '100px';
+  el.style.height = '100%';
+  el.style.zIndex = '10';
+  el.style.transition = '0.5s';
+  // el.innerText = '123'
+  const wrap = document.getElementById('map');
+  wrap.appendChild(el);
+  return el;
+}
