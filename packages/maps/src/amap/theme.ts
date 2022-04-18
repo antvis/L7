f3f3588499e543681fc2e5873eabe6df4f815162
yunji <yunji.me@outlook.com>
2022-04-18 14:13:16 +0800

    chore: 兼容高德地图 V2 矢量瓦片样式

--- packages/maps/src/amap/theme.ts
+++ packages/maps/src/amap/theme.ts
@@ -1,8 +1,8 @@
 export const MapTheme: {
   [key: string]: any;
 } = {
-  dark: 'amap://styles/2a09079c3daac9420ee53b67307a8006?isPublic=true',
-  light: 'amap://styles/1fd9f8ef9751298f11f5c56968312c70?isPublic=true',
+  dark: 'amap://styles/c9f1d10cae34f8ab05e425462c5a58d7?isPublic=true',
+  light: 'amap://styles/c422f5c0cfced5be9fe3a83f05f28a68?isPublic=true',
   normal: 'amap://styles/normal',
   blank: 'amap://styles/07c17002b38775b32a7a76c66cf90e99?isPublic=true',
 };
