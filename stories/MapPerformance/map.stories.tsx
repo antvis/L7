ac5fb0ff79cb7b1cfda18f5bf78a61e8b6821bff
2912401452 <2912401452@qq.com>
2022-04-25 19:07:11 +0800

    feat: 增加顶点更新 demo

--- stories/MapPerformance/map.stories.tsx
+++ stories/MapPerformance/map.stories.tsx
@@ -5,6 +5,7 @@ import UpdateAttrAndEle from './components/updateAttrAndEle';
 import UpdateAttrAndEle_line from './components/updateAttrAndEle_line';
 import UpdateAttrAndEle_polygon from './components/updateAttrAndEle_polygon';
 import UpdateAttrAndEle_planeGeometry from './components/updateAttrAndEle_planeGeometry';
+import UpdateAttrTimeLine from './components/updataPointsTimeLine';
 import PointTest from './components/Map';
 import BigLine from './components/BigLine';
 import DataUpdate from './components/DataUpdate';
@@ -14,6 +15,7 @@ storiesOf('地图性能检测', module)
   .add('更新数据 update line attr&ele', () => <UpdateAttrAndEle_line />)
   .add('更新数据 update polygon attr&ele', () => <UpdateAttrAndEle_polygon />)
   .add('更新数据 update plane geometry attr&ele', () => <UpdateAttrAndEle_planeGeometry />)
+  .add('更新数据 update updateAttrTimeLine', () => <UpdateAttrTimeLine />)
   .add('点', () => <PointTest />)
   .add('BigLine', () => <BigLine />)
   .add('DataUpdate', () => <DataUpdate />);
