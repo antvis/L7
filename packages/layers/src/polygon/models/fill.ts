83b061503a9b11477eede50de8d870acf487694d
2912401452 <2912401452@qq.com>
2022-05-06 16:36:52 +0800

    style: lint style

--- packages/layers/src/polygon/models/fill.ts
+++ packages/layers/src/polygon/models/fill.ts
@@ -1,4 +1,10 @@
-import { AttributeType, gl, IEncodeFeature, IModel, Triangulation } from '@antv/l7-core';
+import {
+  AttributeType,
+  gl,
+  IEncodeFeature,
+  IModel,
+  Triangulation,
+} from '@antv/l7-core';
 import { getMask } from '@antv/l7-utils';
 import { isNumber } from 'lodash';
 import BaseModel from '../../core/BaseModel';
@@ -68,7 +74,7 @@ export default class FillModel extends BaseModel {
   }
 
   public buildModels(): IModel[] {
-    const { frag, vert, triangulation, type }  = this.getModelParams();
+    const { frag, vert, triangulation, type } = this.getModelParams();
     const {
       mask = false,
       maskInside = true,
@@ -79,7 +85,7 @@ export default class FillModel extends BaseModel {
         moduleName: type,
         vertexShader: vert,
         fragmentShader: frag,
-        triangulation: triangulation,
+        triangulation,
         blend: this.getBlend(),
         depth: { enable: false },
         cull: {
@@ -89,29 +95,6 @@ export default class FillModel extends BaseModel {
         stencil: getMask(mask, maskInside),
       }),
     ];
-  }
-
-  private getModelParams(): { frag: string; vert: string; type: string, triangulation: Triangulation } {
-    const {
-      opacityLinear = {
-        enable: false,
-      },
-    } = this.layer.getLayerConfig() as IPolygonLayerStyleOptions;
-    if(opacityLinear.enable) {
-      return {
-        frag: polygon_linear_frag,
-        vert: polygon_linear_vert,
-        type: 'polygon_linear',
-        triangulation: polygonTriangulationWithCenter
-      };
-    } else {
-      return {
-        frag: polygon_frag,
-        vert: polygon_vert,
-        type: 'polygon_fill',
-        triangulation: polygonTriangulation
-      };
-    }
   }
 
   public clearModels() {
@@ -150,6 +133,34 @@ export default class FillModel extends BaseModel {
           },
         },
       });
+    }
+  }
+
+  private getModelParams(): {
+    frag: string;
+    vert: string;
+    type: string;
+    triangulation: Triangulation;
+  } {
+    const {
+      opacityLinear = {
+        enable: false,
+      },
+    } = this.layer.getLayerConfig() as IPolygonLayerStyleOptions;
+    if (opacityLinear.enable) {
+      return {
+        frag: polygon_linear_frag,
+        vert: polygon_linear_vert,
+        type: 'polygon_linear',
+        triangulation: polygonTriangulationWithCenter,
+      };
+    } else {
+      return {
+        frag: polygon_frag,
+        vert: polygon_vert,
+        type: 'polygon_fill',
+        triangulation: polygonTriangulation,
+      };
     }
   }
 }
