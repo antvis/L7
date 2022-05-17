1c3e764c27e6fcc33416c2d82d68de9c861b296b
2912401452 <2912401452@qq.com>
2022-05-17 23:03:37 +0800

    feat: 优化 pickingService 的节流操作

--- packages/core/src/services/interaction/PickingService.ts
+++ packages/core/src/services/interaction/PickingService.ts
@@ -211,12 +211,7 @@ export default class PickingService implements IPickingService {
     this.alreadyInPicking = true;
     const t = new Date().getTime();
     // TODO: 优化拾取操作 在右键时 mousedown 和 contextmenu 几乎同时触发，所以不能舍去这一次的触发
-    if (
-      t - this.lastPickTime > 10 ||
-      target.type === 'contextmenu' ||
-      target.type === 'click' ||
-      target.type === 'mouseup'
-    ) {
+    if (t - this.lastPickTime > 10 || ['contextmenu', 'click', 'mouseup'].includes(target.type)) {
       await this.pickingLayers(target);
     }
     // await this.pickingLayers(target);
