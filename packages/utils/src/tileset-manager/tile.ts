00005f1147842a21917d4057d5b697b465259a46
YiQianYao <42212176+2912401452@users.noreply.github.com>
2022-05-23 18:31:20 +0800

    feat: 发布打包代码调整 (#1117)
    * fix: 修复 mouseup 失效
    
    * chore: update version 2.8.35 -> 2.8.36
    
    * fix: fix bboxPolygon conflict
    
    * fix: fix bboxPolygon conflict
    
    * feat: add utils turf dependencies
    
    * fix: change rollup build config
    
    * feat: 替换 turf/bbox-polygon 的引入方式
    
    * feat: add turf bboxpolygon dependencies
    
    * feat: change TileLoadParams interface export
    
    * style: lint style
    
    * feat: change TilesetManagerOptions interface export
    
    * style: lint style
    
    * feat: rename tilebboxPolygon -> bboxPolygon
    
    * feat: rename tilebboxPolygon -> bboxPolygon
    
    * fix: 解决 rollup 打包问题
    
    Co-authored-by: yunji <yunji.me@outlook.com>

--- packages/utils/src/tileset-manager/tile.ts
+++ packages/utils/src/tileset-manager/tile.ts
@@ -1,26 +1,11 @@
-import { bboxPolygon } from '@turf/turf';
-import { Bounds } from './types';
+import bboxPolygon from '@turf/bbox-polygon';
+import {
+  LoadTileDataStatus,
+  TileBounds,
+  TileLoadDataOptions,
+  TileOptions,
+} from './types';
 import { getTileWarpXY, tileToBounds } from './utils/lonlat-tile';
-
-type TileOptions = { x: number; y: number; z: number; tileSize: number };
-
-export type TileLoadParams = TileOptions & {
-  bounds: Bounds;
-  signal: AbortSignal;
-};
-
-type TileLoadDataOptions = {
-  getData: (tile: TileLoadParams) => Promise<any>;
-  onLoad: (tile: Tile) => void;
-  onError: (error: Error, tile: Tile) => void;
-};
-
-enum LoadTileDataStatus {
-  Loading = 'Loading',
-  Loaded = 'Loaded',
-  Failure = 'Failure',
-  Cancelled = 'Cancelled',
-}
 
 /**
  * 单个瓦片
@@ -98,7 +83,7 @@ export class Tile {
     const [minLng, minLat, maxLng, maxLat] = this.bounds;
     const center = [(maxLng - minLng) / 2, (maxLat - minLat) / 2] as const;
 
-    const polygon = bboxPolygon(this.bounds as Bounds, {
+    const polygon = bboxPolygon(this.bounds as TileBounds, {
       properties: {
         key: this.key,
         bbox: this.bounds,
@@ -158,7 +143,7 @@ export class Tile {
     // 如果请求出错或数据为空
     if (error || !tileData) {
       this.loadStatus = LoadTileDataStatus.Failure;
-      onError(error, this);
+      onError(error as Error, this);
       return;
     }
 
--- packages/utils/src/tileset-manager/types.ts
+++ packages/utils/src/tileset-manager/types.ts
@@ -1,5 +1,7 @@
+import { Tile } from './tile';
+
 // Bounds [minLng, minLat, maxLng, maxLat]
-export type Bounds = [number, number, number, number];
+export type TileBounds = [number, number, number, number];
 
 // 瓦片更新显示策略
 export enum UpdateTileStrategy {
@@ -8,3 +10,33 @@ export enum UpdateTileStrategy {
   // 全部替换策略
   Replace = 'replace',
 }
+
+export type TileOptions = { x: number; y: number; z: number; tileSize: number };
+
+export type TileLoadParams = TileOptions & {
+  bounds: TileBounds;
+  signal: AbortSignal;
+};
+
+export type TileLoadDataOptions = {
+  getData: (tile: TileLoadParams) => Promise<any>;
+  onLoad: (tile: Tile) => void;
+  onError: (error: Error, tile: Tile) => void;
+};
+
+export enum LoadTileDataStatus {
+  Loading = 'Loading',
+  Loaded = 'Loaded',
+  Failure = 'Failure',
+  Cancelled = 'Cancelled',
+}
+
+export type TilesetManagerOptions = {
+  tileSize: number;
+  zoomOffset: number;
+  minZoom: number;
+  maxZoom: number;
+  extent: TileBounds;
+  getTileData: (tile: TileLoadParams) => any;
+  updateStrategy: UpdateTileStrategy | ((tiles: Tile[]) => void);
+};
