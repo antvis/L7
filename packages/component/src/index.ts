9404250c93ee127cdfb92ba2d10ed8c35d496104
yunji <yunji.me@outlook.com>
2022-06-15 19:42:50 +0800

    chore: 透出组件类型定义

--- packages/component/src/index.ts
+++ packages/component/src/index.ts
@@ -1,14 +1,27 @@
 import Control from './control/BaseControl';
-import Layers from './control/layer';
+import Layers, { ILayerControlOption } from './control/layer';
 import Logo from './control/logo';
-import Scale from './control/scale';
-import Zoom from './control/zoom';
+import Scale, { IScaleControlOption } from './control/scale';
+import Zoom, { IZoomControlOption } from './control/zoom';
 import Marker from './marker';
-import MarkerLayer from './markerlayer';
+import MarkerLayer, { IMarkerLayerOption } from './markerlayer';
 import Popup from './popup';
 
 // 引入样式
 // TODO: 使用 Less 或者 Sass，每个组件单独引用自身样式
 import './css/l7.css';
 
-export { Control, Logo, Scale, Zoom, Layers, Marker, Popup, MarkerLayer };
+export {
+  Control,
+  Logo,
+  Scale,
+  IScaleControlOption,
+  Zoom,
+  IZoomControlOption,
+  Layers,
+  ILayerControlOption,
+  Marker,
+  Popup,
+  MarkerLayer,
+  IMarkerLayerOption,
+};
