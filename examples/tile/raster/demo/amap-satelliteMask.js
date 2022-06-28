4cf86b2e58d91260a2cdb9f5dd320a1a2d60bacf
2912401452 <2912401452@qq.com>
2022-06-28 14:35:28 +0800

    style: lint style

--- examples/tile/raster/demo/amap-satelliteMask.js
+++ examples/tile/raster/demo/amap-satelliteMask.js
@@ -5,8 +5,8 @@ const scene = new Scene({
   id: 'map',
   stencil: true,
   map: new Map({
-    center: [120, 30],
-    zoom: 6.5,
+    center: [ 120, 30 ],
+    zoom: 6.5
   })
 });
 
@@ -14,15 +14,15 @@ scene.on('loaded', () => {
 
   fetch(
     // 'https://gw.alipayobjects.com/os/basement_prod/d2e0e930-fd44-4fca-8872-c1037b0fee7b.json',
-    'https://gw.alipayobjects.com/os/bmw-prod/ecd1aaac-44c0-4232-b66c-c0ced76d5c7d.json',
+    'https://gw.alipayobjects.com/os/bmw-prod/ecd1aaac-44c0-4232-b66c-c0ced76d5c7d.json'
   )
-    .then((res) => res.json())
-    .then((data) => {
-       // 影像底图服务
+    .then(res => res.json())
+    .then(data => {
+      // 影像底图服务
       const baseLayer = new RasterLayer({
         zIndex: 1,
         mask: true,
-        maskfence: data,
+        maskfence: data
       });
       baseLayer.source(
         'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
@@ -36,8 +36,8 @@ scene.on('loaded', () => {
       );
 
       scene.addLayer(baseLayer);
-    })
- 
+    });
+
 
   // 影像注记服务
   const annotionLayer = new RasterLayer({
