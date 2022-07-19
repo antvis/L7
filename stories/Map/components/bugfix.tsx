2eb458ac0b11a57a2e10455bbe88577cb80393de
2912401452 <2912401452@qq.com>
2022-07-19 14:16:02 +0800

    style: lint style

--- stories/Map/components/bugfix.tsx
+++ stories/Map/components/bugfix.tsx
@@ -16,31 +16,31 @@ import {
 } from '@antv/l7';
 
 export default class Amap2demo extends React.Component {
-
-
   public async componentDidMount() {
-
-    const source = new Source([
+    const source = new Source(
+      [
+        {
+          lng: 120,
+          lat: 30,
+        },
+      ],
       {
-        lng: 120,
-        lat: 30,
+        parser: {
+          type: 'json',
+          x: 'lng',
+          y: 'lat',
+        },
       },
-    ],
-    {
-      parser: {
-        type: 'json',
-        x: 'lng',
-        y: 'lat',
-      },
-    })
+    );
 
-    
-    let c = 0, scene, layer
+    let c = 0,
+      scene,
+      layer;
     scene = new Scene({
       id: 'map',
       // map: new Mapbox({
-        // map: new GaodeMap({
-        map: new GaodeMapV2({
+      // map: new GaodeMap({
+      map: new GaodeMapV2({
         // map: new Map({
         style: 'dark',
         center: [120, 30],
@@ -49,16 +49,14 @@ export default class Amap2demo extends React.Component {
     });
 
     const layer = new PointLayer()
-    .source(source )
-    .shape('circle')
-    .size(10)
-    .color('#f00');
+      .source(source)
+      .shape('circle')
+      .size(10)
+      .color('#f00');
 
     scene.on('loaded', () => {
       scene.addLayer(layer);
     });
-
-   
   }
 
   public render() {
