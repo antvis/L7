40e29ed8c7e46515bd76123a87f6b93c587f4f04
2912401452 <2912401452@qq.com>
2022-07-26 19:40:01 +0800

    style: lint style

--- packages/utils/src/workers/polygonFillModel.ts
+++ packages/utils/src/workers/polygonFillModel.ts
@@ -1,6 +1,6 @@
-import { IEncodeFeature, IVertexAttributeDescriptor } from './interface';
 import { encodePickingColor } from '../color';
 import { a_Color, a_filter, a_Position, a_vertexId } from './commonFeatureFunc';
+import { IEncodeFeature, IVertexAttributeDescriptor } from './interface';
 import { polygonFillTriangulation as triangulation } from './triangulation';
 
 export const polygonFillModel = async ({
