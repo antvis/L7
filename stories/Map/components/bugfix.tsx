ac35c520fc889f7baa110de4f166f4e91689c899
2912401452 <2912401452@qq.com>
2022-08-02 17:33:52 +0800

    style: lint style

--- stories/Map/components/bugfix.tsx
+++ stories/Map/components/bugfix.tsx
@@ -34,42 +34,47 @@ export default class Amap2demo extends React.Component {
       },
     );
 
-      const scene = new Scene({
-        id: 'map',
-        map: new Mapbox({
-          pitch: 0,
-          style: 'light',
-          center: [ -96, 37.8 ],
-          zoom: 3
-        })
-      });
+    const scene = new Scene({
+      id: 'map',
+      map: new Mapbox({
+        pitch: 0,
+        style: 'light',
+        center: [-96, 37.8],
+        zoom: 3,
+      }),
+    });
 
     scene.on('loaded', () => {
       fetch(
-        'https://gw.alipayobjects.com/os/basement_prod/d36ad90e-3902-4742-b8a2-d93f7e5dafa2.json'
+        'https://gw.alipayobjects.com/os/basement_prod/d36ad90e-3902-4742-b8a2-d93f7e5dafa2.json',
       )
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
           // console.log(data)
-          const color = [ 'rgb(255,255,217)', 'rgb(237,248,177)', 'rgb(199,233,180)', 'rgb(127,205,187)', 'rgb(65,182,196)', 'rgb(29,145,192)', 'rgb(34,94,168)', 'rgb(12,44,132)' ];
+          const color = [
+            'rgb(255,255,217)',
+            'rgb(237,248,177)',
+            'rgb(199,233,180)',
+            'rgb(127,205,187)',
+            'rgb(65,182,196)',
+            'rgb(29,145,192)',
+            'rgb(34,94,168)',
+            'rgb(12,44,132)',
+          ];
           const layer = new PolygonLayer({})
             .source(data)
             .scale('density', {
-              type: 'quantile'
+              type: 'quantile',
             })
-            .color(
-              'density', color
-            )
+            .color('density', color)
             .shape('fill')
             .active(true)
             .style({
-              opacity: 1.0
+              opacity: 1.0,
             });
-        
+
           scene.addLayer(layer);
-          console.log(layer)
-    
-         
+          console.log(layer);
         });
     });
   }
