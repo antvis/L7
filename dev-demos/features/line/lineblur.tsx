9960981bff3b3ff01167558e2280e41ca794b245
shihui <yiqianyao.yqy@alibaba-inc.com>
2022-08-30 18:56:02 +0800

    style: lint style

--- dev-demos/features/line/lineblur.tsx
+++ dev-demos/features/line/lineblur.tsx
@@ -9,26 +9,26 @@ export default () => {
       map: new GaodeMap({
         center: [122, 28],
         zoom: 5,
-        style: 'dark'
+        style: 'dark',
       }),
     });
 
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/55304d8d-4cb0-49e0-9d95-9eeacbe1c80a.json')
-    .then(res => res.json())
-    .then(data => {
-      const blurLine = new LineLayer()
-      .source(data)
-      .size(5)
-      .style({
-        opacity: 0.8,
-        sourceColor: '#f00',
-        targetColor: '#ff0',
-        linearDir: 'horizontal',
-      })
-      scene.addLayer(blurLine);
-    })
-
-  
+    fetch(
+      'https://gw.alipayobjects.com/os/bmw-prod/55304d8d-4cb0-49e0-9d95-9eeacbe1c80a.json',
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const blurLine = new LineLayer()
+          .source(data)
+          .size(5)
+          .style({
+            opacity: 0.8,
+            sourceColor: '#f00',
+            targetColor: '#ff0',
+            linearDir: 'horizontal',
+          });
+        scene.addLayer(blurLine);
+      });
   }, []);
   return (
     <div
