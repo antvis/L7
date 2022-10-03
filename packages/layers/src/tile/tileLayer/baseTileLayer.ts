283602d059ba33b180bd84b5aaa383ac3711b20c
shihui <yiqianyao.yqy@alibaba-inc.com>
2022-10-03 20:21:44 +0800

    fix: 修复瓦片图层事件失效

--- packages/layers/src/tile/tileLayer/baseTileLayer.ts
+++ packages/layers/src/tile/tileLayer/baseTileLayer.ts
@@ -249,6 +249,7 @@ export default class BaseTileLayer implements ITileLayer {
      * layer.on('unpick', (ev) => {}); // 图层外的操作的所有事件
      */
     this.parent.on('subLayerClick', (e) => {
+      console.log('subLayerClick')
       this.parent.emit('click', { ...e });
     });
     this.parent.on('subLayerMouseMove', (e) =>
