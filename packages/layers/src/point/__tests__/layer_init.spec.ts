93b0160a13a286aa28d751f3548c4aa18aa42e7d
shihui <yiqianyao.yqy@alibaba-inc.com>
2022-10-19 09:54:07 +0800

    style: lint style

--- packages/layers/src/point/__tests__/layer_init.spec.ts
+++ packages/layers/src/point/__tests__/layer_init.spec.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-// import { TestScene } from '@antv/l7-test-utils';
-// import PointLayer from '../';
+import { TestScene } from '@antv/l7-test-utils';
+import PointLayer from '../';
 
 describe('template', () => {
 
@@ -10,42 +10,42 @@ describe('template', () => {
     
   });
 
-  // const el = document.createElement('div');
-  // el.id = 'test-div-id';
-  // const body = document.querySelector('body') as HTMLBodyElement;
-  // body.appendChild(el);
-  // const scene = TestScene({});
-  // const testData =  [
-  //   {
-  //     x: 112,
-  //     y: 30,
-  //     name:'text1',
-  //     v:10,
-  //   },
-  //   {
-  //     x: 112,
-  //     y: 32,
-  //     name:'text2',
-  //     v:20
-  //   },
-  // ];
+  const el = document.createElement('div');
+  el.id = 'test-div-id';
+  const body = document.querySelector('body') as HTMLBodyElement;
+  body.appendChild(el);
+  const scene = TestScene({});
+  const testData =  [
+    {
+      x: 112,
+      y: 30,
+      name:'text1',
+      v:10,
+    },
+    {
+      x: 112,
+      y: 32,
+      name:'text2',
+      v:20
+    },
+  ];
 
-  // it('scene layer fill', async () => {
-  //   const layer = new PointLayer().source(
-  //       testData,
-  //     {
-  //       parser: {
-  //         type: 'json',
-  //         x: 'x',
-  //         y: 'y',
-  //       },
-  //     },
-  //   ).shape('circle')
-  //   .color('red')
-  //   .size(10)
-  //   scene.addLayer(layer)
+  it('scene layer fill', async () => {
+    const layer = new PointLayer().source(
+        testData,
+      {
+        parser: {
+          type: 'json',
+          x: 'x',
+          y: 'y',
+        },
+      },
+    ).shape('circle')
+    .color('red')
+    .size(10)
+    scene.addLayer(layer)
     
-  // });
+  });
   // it('scene layer text', async () => {
   //   const layer = new PointLayer({name:'text'}).source(
   //       testData,
