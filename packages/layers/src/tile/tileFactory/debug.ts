49d37ba9bf14917891adbeefa382990a738dd9f1
shihui <yiqianyao.yqy@alibaba-inc.com>
2022-10-20 11:01:54 +0800

    feat: change file name

--- packages/layers/src/tile/tileFactory/debug.ts
+++ packages/layers/src/tile/tileFactory/debug.ts
@@ -2,7 +2,7 @@ import { ILayer, ISubLayerInitOptions } from '@antv/l7-core';
 import { SourceTile } from '@antv/l7-utils';
 import { ITileFactoryOptions } from '../interface';
 import TileFactory from './base';
-import VectorLayer from './layers/VectorLayer';
+import VectorLayer from './layers/vectorLayer';
 
 export default class TestTile extends TileFactory {
   public parentLayer: ILayer;
