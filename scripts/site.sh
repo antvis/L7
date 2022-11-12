0996d3d259b91e200618c450275b0f3e12224fd5
lzxue <lzx199065@gmail.com>
2022-11-12 10:37:47 +0800

    fix: dumi 版本锁定

--- scripts/site.sh
+++ scripts/site.sh
@@ -8,10 +8,18 @@ cd ./site_temp
 
 git clone  https://code.alipay.com/datavis/l7-site.git --depth=1
 
+cd l7-site
 
-rsync --exclude packages/site/node_modules  -r packages/site ./temp 
+git checkout -b feat_update_site
 
 
-rm -rf ./temp/site
 
-rsync -av  --exclude .dumi/tmp  --exclude node_modules --exclude public  packages/site ./site_temp/l7-site
+rsync -av  --exclude .dumi/tmp  --exclude node_modules --exclude public  --exclude dist packages/site/* ./site_temp/l7-site
+
+git add .
+
+git commit -m 'docs: 更新网站'
+
+git push --set-upstream origin feat_update_site
+
+echo '代码已提交'
