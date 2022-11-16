45a5fe03751fe277514bee011e43345101a65482
lzxue <lzx199065@gmail.com>
2022-11-16 15:59:44 +0800

    fix: site 配置

--- scripts/site.sh
+++ scripts/site.sh
@@ -9,17 +9,23 @@ cd ./site_temp
 git clone  https://code.alipay.com/datavis/l7-site.git --depth=1
 
 cd l7-site
+echo `pwd`
 
-git checkout -b feat_update_site
+feat_name=feat_update_site_`date +%Y%m%d%M%S`;
+echo $feat_name
+git checkout -b $feat_name
 
+cd ../../
+echo `pwd`
+rsync -av   --exclude .dumi/tmp-production --exclude .dumi/tmp  --exclude node_modules --exclude public  --exclude dist ./packages/site/ ./site_temp/l7-site/
 
-
-rsync -av  --exclude .dumi/tmp  --exclude node_modules --exclude public  --exclude dist packages/site/* ./site_temp/l7-site
+cd ./site_temp/l7-site
 
 git add .
 
+
 git commit -m 'docs: 更新网站'
 
-git push --set-upstream origin feat_update_site
+git push --set-upstream origin $feat_name
 
 echo '代码已提交'
