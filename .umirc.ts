a51f08be2ab00dff9847167399a99dcc2458a6bc
lzxue <lzx199065@gmail.com>
2022-11-21 22:57:03 +0800

    fix: 更新dumi 配置

--- .umirc.ts
+++ .umirc.ts
@@ -96,6 +96,13 @@ export default defineConfig({
     /** lodash */
     'https://gw.alipayobjects.com/os/lib/lodash/4.17.20/lodash.min.js',
   ],
+  internalSite: {
+    url: 'https://s2.antv.antgroup.com',
+    name: {
+      zh: '国内镜像',
+      en: 'China Mirror',
+    },
+  },
 
   // more config: https://d.umijs.org/config
 });
