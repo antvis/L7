777c291a5f8b8da8f9d048c7659bcd1bc2cb9b7a
lzxue <120635640@qq.com>
2023-03-09 20:42:21 +0800

    feat: 添加数据版本

--- packages/site/maptools/demo/util.ts
+++ packages/site/maptools/demo/util.ts
@@ -95,6 +95,13 @@ export const editionOptions = {
   RDBSource: [
     { value: '2023', label: '2023' },
     { value: '2022', label: '2022' },
+    { value: '2021', label: '2021' },
+    { value: '2020', label: '2020' },
+    { value: '2019', label: '2019' },
+    { value: '2018', label: '2018' },
+    { value: '2017', label: '2017' },
+    { value: '2016', label: '2016' },
+    { value: '2015', label: '2015' },
   ],
 };
 
