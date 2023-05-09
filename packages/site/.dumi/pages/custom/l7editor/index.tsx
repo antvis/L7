61595549e8e7aa67758c27662951051b1e4174ae
yanxiong <oujinhui.ojh@antgroup.com>
2023-05-09 13:37:47 +0800

    docs: 在地图工具菜单项新增 L7Editor

---
+++ packages/site/.dumi/pages/custom/l7editor/index.tsx
@@ -0,0 +1,18 @@
+import { Footer } from '@antv/dumi-theme-antv/dist/slots/Footer';
+import { Header } from '@antv/dumi-theme-antv/dist/slots/Header';
+import React, { useRef } from 'react';
+const L7Editor: React.FC = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      <Header />
+      <iframe
+        src="https://l7editor.antv.antgroup.com/"
+        height="650"
+        width="100%"
+      />
+      <Footer />
+    </div>
+  );
+};
+
+export default L7Editor;
