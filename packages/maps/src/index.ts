6878796faf355504d4002d264200be39bfc0db2d
@thinkinggis <lzx199065@gmail.com>
2023-10-09 20:44:40 +0800

    chore: umd 打包 (#1910)

--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -1,11 +1,8 @@
 import GaodeMapV1 from './amap/';
-// import GaodeMapV1 from './amap/';
 import { default as GaodeMap, default as GaodeMapV2 } from './amap2/';
 import Earth from './earth/';
-// import GaodeMapV2 from './amap2/';
 import Map from './map/';
 import Mapbox from './mapbox/';
 import { Version } from './version';
-
 export * from './utils';
 export { Version, GaodeMap, GaodeMapV1, GaodeMapV2, Mapbox, Map, Earth };
