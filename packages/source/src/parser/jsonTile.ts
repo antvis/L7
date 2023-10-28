2503e7532d74f936febbd9c9e99d8f51d300bee9
Kip <62598129+KipSong@users.noreply.github.com>
2023-10-28 12:17:22 +0800

    JSON Tile Parser (#1970)
    * feat: add tile_json_parser
    
    * feat: update lint
    
    ---------
    
    Co-authored-by: kip.song <kip.song@nio.com>

---
+++ packages/source/src/parser/jsonTile.ts
@@ -0,0 +1,106 @@
+import {
+  getData,
+  getURLFromTemplate,
+  RequestParameters,
+  SourceTile,
+  TileLoadParams,
+} from '@antv/l7-utils';
+import { Feature } from '@turf/helpers';
+import { IParserData, ITileSource } from '../interface';
+import VtSource from '../source/geojsonvt';
+
+import { ITileParserCFG } from '@antv/l7-core';
+
+export type MapboxVectorTile = {
+  layers: { [key: string]: { features: Feature[] } };
+};
+
+const getVectorTile = async (
+  url: string,
+  tile: SourceTile,
+  requestParameters?: Partial<RequestParameters>,
+  getCustomData?: ITileParserCFG['getCustomData'],
+): Promise<ITileSource> => {
+  const params = { x: tile.x, y: tile.y, z: tile.z };
+  const tileUrl = getURLFromTemplate(url, params);
+  return new Promise((resolve) => {
+    if (getCustomData) {
+      getCustomData(params, (err, data) => {
+        if (err || !data) {
+          const vectorTile: MapboxVectorTile = {
+            layers: { defaultLayer: { features: [] } },
+          };
+          const vectorSource = new VtSource(vectorTile, tile.x, tile.y, tile.z);
+          resolve(vectorSource);
+        } else {
+          const vectorTile: MapboxVectorTile = {
+            layers: { defaultLayer: { features: data.features } },
+          };
+          const vectorSource = new VtSource(vectorTile, tile.x, tile.y, tile.z);
+          resolve(vectorSource);
+        }
+      });
+    } else {
+      getData(
+        {
+          ...requestParameters,
+          url: tileUrl,
+        },
+        (err, data) => {
+          if (err || !data) {
+            const vectorTile: MapboxVectorTile = {
+              layers: {
+                defaultLayer: {
+                  features: [],
+                },
+              },
+            };
+            const vectorSource = new VtSource(
+              vectorTile,
+              tile.x,
+              tile.y,
+              tile.z,
+            );
+            resolve(vectorSource);
+          } else {
+            const json = JSON.parse(data);
+            const vectorTile: MapboxVectorTile = {
+              layers: {
+                defaultLayer: {
+                  features: json,
+                },
+              },
+            };
+            const vectorSource = new VtSource(
+              vectorTile,
+              tile.x,
+              tile.y,
+              tile.z,
+            );
+            resolve(vectorSource);
+          }
+        },
+      );
+    }
+  });
+};
+
+export default function jsonTile(
+  url: string,
+  cfg: ITileParserCFG,
+): IParserData {
+  const getTileData = (_: TileLoadParams, tile: SourceTile) => {
+    return getVectorTile(url, tile, cfg?.requestParameters, cfg.getCustomData);
+  };
+
+  const tilesetOptions = {
+    ...cfg,
+    getTileData,
+  };
+
+  return {
+    dataArray: [],
+    tilesetOptions,
+    isTile: true,
+  };
+}
