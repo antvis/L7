f38ac2b0bae59c39bbf33d9930d3fb171c06dbc9
yunji <yunji.me@outlook.com>
2023-11-03 20:20:29 +0800

    feat: 允许图片图层请求数据带参数

--- packages/source/src/parser/image.ts
+++ packages/source/src/parser/image.ts
@@ -1,19 +1,23 @@
 import { IParserData } from '@antv/l7-core';
-import { getImage, isImageBitmap } from '@antv/l7-utils';
+import { RequestParameters, getImage, isImageBitmap } from '@antv/l7-utils';
 interface IImageCfg {
   extent: [number, number, number, number];
+  requestParameters?: Omit<RequestParameters, 'url'>;
 }
 export default function image(
   data: string | string[] | HTMLImageElement | ImageBitmap,
   cfg: IImageCfg,
 ): IParserData {
   // 为 extent 赋默认值
-  const { extent = [121.168, 30.2828, 121.384, 30.4219] } = cfg;
+  const {
+    extent = [121.168, 30.2828, 121.384, 30.4219],
+    requestParameters = {},
+  } = cfg;
   const images = new Promise((resolve) => {
     if (data instanceof HTMLImageElement || isImageBitmap(data)) {
       resolve([data]);
     } else {
-      loadData(data, (res: any) => {
+      loadData(data, requestParameters, (res: any) => {
         resolve(res);
       });
     }
@@ -35,11 +39,14 @@ export default function image(
   return resultData;
 }
 
-function loadData(data: string | string[], done: any) {
-  const url = data;
+function loadData(
+  url: string | string[],
+  requestParameters: Omit<RequestParameters, 'url'>,
+  done: any,
+) {
   const imageDatas: Array<HTMLImageElement | ImageBitmap> = [];
   if (typeof url === 'string') {
-    getImage({ url }, (err, img) => {
+    getImage({ ...requestParameters, url }, (err, img) => {
       if (img) {
         imageDatas.push(img);
         done(imageDatas);
@@ -49,7 +56,7 @@ function loadData(data: string | string[], done: any) {
     const imageCount = url.length;
     let imageindex = 0;
     url.forEach((item) => {
-      getImage({ url: item }, (err, img) => {
+      getImage({ ...requestParameters, url: item }, (err, img) => {
         imageindex++;
         if (img) {
           imageDatas.push(img);
