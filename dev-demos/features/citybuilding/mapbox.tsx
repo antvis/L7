500939c54931cb2b04dc32f3d5e1993366ee8484
huyang <huyang@uinnova.com>
2023-12-10 12:51:46 +0800

    chore: update citybuilding to Device Engine

--- dev-demos/features/citybuilding/mapbox.tsx
+++ dev-demos/features/citybuilding/mapbox.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { CityBuildingLayer, LineLayer, PolygonLayer, Scene } from '@antv/l7';
+import { CityBuildingLayer, Scene } from '@antv/l7';
 // @ts-ignore
 import { MapLibre } from '@antv/l7-maps';
 import React, { useEffect } from 'react';
@@ -46,71 +46,71 @@ export default () => {
       scene.addLayer(pointLayer);
     });
 
-    fetch(
-      'https://gw.alipayobjects.com/os/bmw-prod/67130c6c-7f49-4680-915c-54e69730861d.json',
-    )
-      .then((data) => data.json())
-      .then(({ lakeBorderData, lakeData, landData }) => {
-        const lakeLayer = new PolygonLayer()
-          .source(lakeData)
-          .shape('fill')
-          .color('#1E90FF')
-          .style({
-            opacity: 0.4,
-            opacityLinear: {
-              enable: true,
-              dir: 'out', // in - out
-            },
-          });
-        const landLayer = new PolygonLayer()
-          .source(landData)
-          .shape('fill')
-          .color('#3CB371')
-          .style({
-            opacity: 0.4,
-            opacityLinear: {
-              enable: true,
-              dir: 'in', // in - out
-            },
-          });
+    // fetch(
+    //   'https://gw.alipayobjects.com/os/bmw-prod/67130c6c-7f49-4680-915c-54e69730861d.json',
+    // )
+    //   .then((data) => data.json())
+    //   .then(({ lakeBorderData, lakeData, landData }) => {
+    //     const lakeLayer = new PolygonLayer()
+    //       .source(lakeData)
+    //       .shape('fill')
+    //       .color('#1E90FF')
+    //       .style({
+    //         opacity: 0.4,
+    //         opacityLinear: {
+    //           enable: true,
+    //           dir: 'out', // in - out
+    //         },
+    //       });
+    //     const landLayer = new PolygonLayer()
+    //       .source(landData)
+    //       .shape('fill')
+    //       .color('#3CB371')
+    //       .style({
+    //         opacity: 0.4,
+    //         opacityLinear: {
+    //           enable: true,
+    //           dir: 'in', // in - out
+    //         },
+    //       });
 
-        const lakeBorderLayer = new PolygonLayer()
-          .source(lakeBorderData)
-          .shape('fill')
-          .color('#ccc')
-          .style({
-            opacity: 0.5,
-            opacityLinear: {
-              enable: true,
-              dir: 'in', // in - out
-            },
-          });
+    //     const lakeBorderLayer = new PolygonLayer()
+    //       .source(lakeBorderData)
+    //       .shape('fill')
+    //       .color('#ccc')
+    //       .style({
+    //         opacity: 0.5,
+    //         opacityLinear: {
+    //           enable: true,
+    //           dir: 'in', // in - out
+    //         },
+    //       });
 
-        scene.addLayer(lakeLayer);
-        scene.addLayer(lakeBorderLayer);
-        scene.addLayer(landLayer);
-      });
+    //     scene.addLayer(lakeLayer);
+    //     scene.addLayer(lakeBorderLayer);
+    //     scene.addLayer(landLayer);
+    //   });
 
-    fetch(
-      'https://gw.alipayobjects.com/os/basement_prod/40ef2173-df66-4154-a8c0-785e93a5f18e.json',
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const layer = new LineLayer({
-          zIndex: 0,
-          depth: true,
-        })
-          .source(data)
-          .size(1)
-          .shape('line')
-          .color('#1990FF')
-          .animate({
-            interval: 1, // 间隔
-            duration: 2, // 持续时间，延时
-            trailLength: 2, // 流线长度
-          });
-        scene.addLayer(layer);
-      });
+    // fetch(
+    //   'https://gw.alipayobjects.com/os/basement_prod/40ef2173-df66-4154-a8c0-785e93a5f18e.json',
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     const layer = new LineLayer({
+    //       zIndex: 0,
+    //       depth: true,
+    //     })
+    //       .source(data)
+    //       .size(1)
+    //       .shape('line')
+    //       .color('#1990FF')
+    //       .animate({
+    //         interval: 1, // 间隔
+    //         duration: 2, // 持续时间，延时
+    //         trailLength: 2, // 流线长度
+    //       });
+    //     scene.addLayer(layer);
+    //   });
   }, []);
   return (
     <div
