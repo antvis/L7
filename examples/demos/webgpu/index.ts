c379f06a1e45a8ca9c29e659a7806683e70bc65a
yuqi.pyq <yuqi.pyq@antgroup.com>
2024-01-02 10:19:13 +0800

    fix: remove redundant varying in extrude vert shader

--- examples/demos/webgpu/index.ts
+++ examples/demos/webgpu/index.ts
@@ -1,6 +1,7 @@
 export { MapRender as boids } from './boids';
 export { MapRender as compute_texture } from './compute_texture';
 export { MapRender as WebGL_IDW } from './idw';
+export { MapRender as point_column } from './point_column';
 export { MapRender as point_fill } from './point_fill';
 export { MapRender as point_image } from './point_image';
 export { MapRender as texture } from './texture';
