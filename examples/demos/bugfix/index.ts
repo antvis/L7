ea3e80965cbd2922d243f2ec6b3611531515b21d
lzxue <120635640@qq.com>
2024-03-04 19:00:31 +0800

    docs: 添加text offset demo

--- examples/demos/bugfix/index.ts
+++ examples/demos/bugfix/index.ts
@@ -3,3 +3,4 @@ export { MapRender as size } from './size';
 export { MapRender as touchend } from './touchend';
 export { MapRender as polygon } from './polygon';
 export { MapRender as tile_update } from './tile_update';
+export { MapRender as text } from './text_offsets';
---
+++ examples/demos/bugfix/text_offsets.ts
@@ -0,0 +1,58 @@
+import { PointLayer, Scene } from '@antv/l7';
+import * as allMap from '@antv/l7-maps';
+
+export function MapRender(option: { map: string; renderer: string }) {
+  const scene = new Scene({
+    id: 'map',
+    renderer: option.renderer,
+    map: new allMap[option.map || 'Map']({
+      style: 'light',
+      center: [121.434765, 31.256735],
+      zoom: 14.83,
+    }),
+  });
+  scene.on('loaded', () => {
+    fetch(
+      'https://gw.alipayobjects.com/os/basement_prod/893d1d5f-11d9-45f3-8322-ee9140d288ae.json',
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const circleLayer = new PointLayer()
+        .source(data, {
+          parser: {
+            type: 'json',
+            x: 'longitude',
+            y: 'latitude',
+          },
+        })
+        .shape('circle')
+        .color('#0f0')
+        .size(5)
+        const imageLayerText = new PointLayer()
+          .source(data, {
+            parser: {
+              type: 'json',
+              x: 'longitude',
+              y: 'latitude',
+            },
+          })
+          .shape('name', 'text')
+          .color('#f00')
+          .size(10)
+          .style({
+            textOffset: {
+              field: 'count',
+              value: (v)=>{
+                return [0, v*14];
+              },
+            },
+          });
+        scene.addLayer(imageLayerText);
+        scene.addLayer(circleLayer);
+
+        if (window['screenshot']) {
+          window['screenshot']();
+        }
+      });
+  });
+}
