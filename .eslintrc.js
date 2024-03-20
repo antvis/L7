ff375cffe7109109fbad5c06659008ace75a7320
yunji <yunji.me@outlook.com>
2024-03-20 21:05:13 +0800

    chore: add lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,48 @@
 module.exports = {
-    globals: {
-        'AMap': true,
-        'L7': true,
+  root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    AMap: true,
+    L7: true,
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    'no-unsafe-optional-chaining': 'warn',
+    'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/no-loss-of-precision': 0,
+    '@typescript-eslint/no-inferrable-types': 0,
+    'no-constant-condition': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    'no-prototype-builtins': 0,
+    'no-case-declarations': 0,
+    'no-useless-catch': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-duplicate-enum-values': 0,
+    'prefer-rest-params': 0,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-        jest: true,
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module'
-    },
-    plugins: ['@typescript-eslint','unused-imports'],
-    rules: {
-        '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-        'no-unsafe-optional-chaining':'warn',
-        'unused-imports/no-unused-imports': 'error',
-        '@typescript-eslint/no-loss-of-precision':0,
-        '@typescript-eslint/no-inferrable-types': 0,
-        'no-constant-condition': 0,
-        '@typescript-eslint/ban-types': 0,
-        '@typescript-eslint/ban-ts-comment': 0,
-        '@typescript-eslint/no-empty-function': 0,
-        'no-prototype-builtins': 0,
-        'no-case-declarations': 0,
-        'no-useless-catch': 0,
-        '@typescript-eslint/explicit-module-boundary-types': 0,
-        '@typescript-eslint/no-explicit-any':0,
-        '@typescript-eslint/no-duplicate-enum-values':0,
-        'prefer-rest-params':0,
-    },
-    settings: {
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-        'import/resolver': {
-            typescript: {
-                alwaysTryTypes: true,
-            },
-        },
-    }
+  },
 };
