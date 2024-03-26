dc4a31e61b2c1fac1614a95a3d69aa91f30a8f4d
yunji <yunji.me@outlook.com>
2024-03-26 13:50:26 +0800

    chore: root config

--- scripts/format-check.sh
+++ scripts/format-check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx prettier --check \"./**/*.{ts,tsx,js,jsx,json,md,css,less,glsl}\"
+npx prettier . --check \"./**/*.{ts,tsx,js,jsx,json,md,css,less,glsl}\"
--- scripts/format-fix.sh
+++ scripts/format-fix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx prettier --write \"./**/*.{ts,tsx,js,jsx,json,md,css,less,glsl}\"
+npx prettier . --write \"./**/*.{ts,tsx,js,jsx,json,md,css,less,glsl}\"
