3403053c26726955096d07c9a7e4a1053819c89c
yunji <yunji.me@outlook.com>
2024-03-29 19:50:01 +0800

    test: vector tile case

--- packages/layers/__tests__/tile/raster-tile.spec.ts
+++ packages/layers/__tests__/tile/raster-tile.spec.ts
@@ -1,12 +1,11 @@
 import { TestScene } from '@antv/l7-test-utils';
 import RasterLayer from '../../src/raster/index';
 
-describe('RasterLayer', () => {
+describe('raster-tile', () => {
   let scene: any;
   beforeEach(() => {
     scene = TestScene();
   });
-  // vector data
   it('rasterLayer raster', async () => {
     const url1 =
       'https://tiles{1-3}.geovisearth.com/base/v1/ter/{z}/{x}/{y}?format=webp&tmsIds=w&token=b2a0cfc132cd60b61391b9dd63c15711eadb9b38a9943e3f98160d5710aef788';
