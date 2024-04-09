b2f4522b8ce4025f1f5a5e13add4e2ba15ed4620
yunji <yunji.me@outlook.com>
2024-04-09 17:14:14 +0800

    test: add heatmap snapshot

---
+++ __tests__/integration/heatmap.spec.ts
@@ -0,0 +1,19 @@
+import { generateCanvasTestCases } from './utils/generator';
+
+const TEST_CASES = [
+  {
+    name: 'normal',
+  },
+  {
+    name: 'grid',
+    snapshots: false,
+  },
+  {
+    name: 'hexagon',
+    snapshots: false,
+  },
+];
+
+describe('Heatmap Snapshot', () => {
+  generateCanvasTestCases('Heatmap', TEST_CASES);
+});
