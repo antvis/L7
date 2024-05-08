50e7063e1ceba479168a5549f40de9e279c59e14
heiyexing <oujinhui.ojh@antgroup.com>
2024-05-08 10:09:17 +0800

    fix: 修复 LayerPopup followCursor 默认值问题 (#2446)

--- examples/demos/components/index.ts
+++ examples/demos/components/index.ts
@@ -1,3 +1,4 @@
 export { MapRender as control } from './control';
+export { MapRender as layerPopup } from './layerPopup';
 export { MapRender as marker } from './marker';
 export { MapRender as swipe } from './swipe';
