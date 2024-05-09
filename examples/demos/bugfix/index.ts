ee64cefa5d7dc67ea799e3fb26ae39d88e3c3312
lvisei <yunji.me@outlook.com>
2024-05-09 14:02:39 +0800

    fix: device 渲染模式下 setData 时不生效 (#2447)
    * fix: device 渲染模式下 setData 时不生效
    
    * fix: 避免 gl.deleteVertexArray 删除后，后续不能重复使用
    
    * chore: 关闭 vao 缓存

--- examples/demos/bugfix/index.ts
+++ examples/demos/bugfix/index.ts
@@ -1,7 +1,7 @@
 export { MapRender as color } from './color';
 export { MapRender as event_legend } from './event_legend';
-export { MapRender as polygon } from './polygon';
 export { MapRender as remove_muti_layer } from './remove-muti-layer';
+export { MapRender as setData } from './set-data';
 export { MapRender as size } from './size';
 export { MapRender as text } from './text_offsets';
 export { MapRender as tile_update } from './tile_update';
