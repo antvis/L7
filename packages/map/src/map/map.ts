f57ea4a6c1b78c884c617bb58ad5bec26a5b66bb
zhanggy <geodaoyu@foxmail.com>
2025-07-08 14:57:58 +0800

    Fix: simple map location (#2734)
    * feat: add simple-map to demos/bugfix
    
    * chore: missing @eslint/js
    
    * fix: 简单坐标系的经纬度转换
    
    * fix: 补充version和mapSize的注释
    
    * docs: typo err in simple_coordinates.zh.md
    
    ---------
    
    Co-authored-by: zhanggy <zhanggy@geoscene.cn>

--- packages/map/src/map/map.ts
+++ packages/map/src/map/map.ts
@@ -35,6 +35,16 @@ import type { TaskID } from './util/task_queue';
  */
 export type MapOptions = {
   /**
+   * The MapType of the map.
+   * @defaultValue DEFAUlTMAP
+   */
+  version?: string;
+  /**
+   * The map size for SIMPLE map.
+   * @defaultValue 10000
+   */
+  mapSize?: number;
+  /**
    * If `false`, no mouse, touch, or keyboard listeners will be attached to the map, so it will not respond to interaction.
    * @defaultValue true
    */
@@ -266,6 +276,9 @@ export class Map extends Camera {
   _removed: boolean;
   _clickTolerance: number;
 
+  version: string;
+  mapSize: number;
+
   /**
    * The map's {@link ScrollZoomHandler}, which implements zooming in and out with a scroll wheel or trackpad.
    * Find more details and examples using `scrollZoom` in the {@link ScrollZoomHandler} section.
@@ -362,6 +375,8 @@ export class Map extends Camera {
     this._bearingSnap = resolvedOptions.bearingSnap!;
     this._fadeDuration = resolvedOptions.fadeDuration!;
     this._clickTolerance = resolvedOptions.clickTolerance!;
+    this.version = options.version;
+    this.mapSize = options.mapSize;
 
     if (typeof resolvedOptions.container === 'string') {
       this._container = document.getElementById(resolvedOptions.container)!;
